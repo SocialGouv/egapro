3b2a49d9022690aa624a8b2862caafa87a573822
freddy <freddy03h@gmail.com>
2019-04-08 17:30:58 +0200

    add emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <header css={styles.header}>
+        <p>EGAPRO - Prototype</p>
+      </header>
+    </div>
+  );
 }
+
+const styles = {
+  header: css({
+    backgroundColor: "#282c34",
+    minHeight: "10vh",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "calc(10px + 2vmin)",
+    color: "white",
+    textAlign: "center"
+  })
+};
 
 export default App;
