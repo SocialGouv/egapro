be314d2d416d4ccf31c96646cb4376fd16f21fde
Nicolas Bovorasmy <nbovorasmy@octo.com>
2019-06-10 21:28:30 +0200

    ci: change pod init kinto to job

--- k8s/scripts/init-kinto.sh
+++ k8s/scripts/init-kinto.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-INIT_KINTO_POD_STATUS=$(kubectl get pod egapro-init-kinto"$1")
+INIT_KINTO_POD_STATUS=$(kubectl get job egapro-init-kinto"$1")
 
-# Check if egapro-init-kinto pod exists
+# Check if egapro-init-kinto job exists
 if [ ! "$INIT_KINTO_POD_STATUS" ]
 then
-    kubectl apply -f k8s/kinto/pod-init-kinto-egapro.yml
+    kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml
 else
-    kubectl delete pod egapro-init-kinto"$1"
-    kubectl apply -f k8s/kinto/pod-init-kinto-egapro.yml
+    kubectl delete job egapro-init-kinto"$1"
+    kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml
 fi
 
