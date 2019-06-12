8c6fa2e302d0a2c18f249e2f9d5b2723aec602f1
Aurélie Rolland <51441028+Auremie@users.noreply.github.com>
2019-06-12 11:19:04 +0200

    wording

--- packages/app/src/views/Home.tsx
+++ packages/app/src/views/Home.tsx
@@ -8,8 +8,17 @@ import ButtonLink from "../components/ButtonLink";
 function Home(props: RouteComponentProps) {
   return (
     <Page
-      title="Bienvenue sur Egapro"
-      tagline="L’index de l’égalité a été conçu pour faire progresser au sein des entreprises l’égalité entre les hommes et les femmes. Il permet de mesurer où en sont les entreprises sur le sujet, mettre en évidence leurs points de progression et mettre en œuvre les mesures correctives nécessaires. en savoir plus L’outil EgaPro permet aux entreprises de calculer leur index d’égalité professionnelle de façon simple, rapide et en profitant d’une aide en ligne sur les détails du calcul de chaque indicateur et sur les questions les plus fréquemment posées."
+      title="Bienvenue sur l'outil Egapro"
+      tagline="L’index de l’égalité professionnelle a été conçu 
+               pour faire progresser au sein des entreprises l’égalité salariale 
+               entre les femmes et les hommes.
+               <br/>Il permet aux entreprises de mesurer, en toute transparence, 
+               les écarts de rémunération entre les sexes et de mettre en évidence 
+               leurs points de progression. Lorsque des disparités salariales sont constatées, 
+               des mesures de correction doivent être prises.
+               <br/>Avec l’outil de simulation en ligne, calculez votre Index de façon simple et rapide, 
+               tout en profitant d’une aide sur les détails du calcul de chaque indicateur et sur les questions 
+               les plus fréquemment posées. "
     >
       <div css={styles.action}>
         <ButtonLink to="/simulateur" label="commencer la simulation" />
