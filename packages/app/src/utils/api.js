8987546a23e9338c97351f3c30c89d5a17448d2e
freddy <freddy03h@gmail.com>
2019-06-12 15:04:27 +0200

    start api

---
+++ packages/app/src/utils/api.js
@@ -0,0 +1,68 @@
+const origin = process.env.REACT_APP_API_URL + "/api";
+
+const commonHeaders = {
+  Accept: "application/json"
+};
+
+const commonContentHeaders = {
+  ...commonHeaders,
+  "Content-Type": "application/json"
+};
+
+////////////
+
+class ApiError extends Error {
+  constructor(response, jsonBody, ...params) {
+    super(...params);
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ApiError);
+    }
+    this.response = response;
+    this.jsonBody = jsonBody;
+  }
+}
+
+function checkStatusAndParseJson(response) {
+  if (response.status === 204) {
+    return { response };
+  }
+
+  let jsonPromise = response.json(); // there's always a body
+  if (response.status >= 200 && response.status < 300) {
+    return jsonPromise.then(jsonBody => ({ response, jsonBody }));
+  } else {
+    return jsonPromise.then(jsonBody => {
+      const apiError = new ApiError(response, jsonBody);
+      return Promise.reject.bind(Promise)(apiError);
+    });
+  }
+}
+
+/////////////
+
+function fetchResource(method, pathname, body) {
+  const requestObj = {
+    method,
+    headers: body ? commonContentHeaders : commonHeaders,
+    body: body ? JSON.stringify(body) : undefined
+  };
+
+  return fetch(origin + pathname, requestObj).then(checkStatusAndParseJson);
+}
+
+const getResource = pathname => fetchResource("GET", pathname);
+const postResource = (pathname, body) => fetchResource("POST", pathname, body);
+const putResource = (pathname, body) => fetchResource("PUT", pathname, body);
+
+/////////////
+
+export const readIndicatorsDatas = id => getResource(`/indicators-datas/${id}`);
+
+export const createIndicatorsDatas = data =>
+  postResource("/indicators-datas", data);
+
+export const updateIndicatorsDatas = (id, data) =>
+  putResource(`/indicators-datas`, { id, data });
+
+export const sendEmailIndicatorsDatas = (id, email) =>
+  putResource(`/indicators-datas/${id}/emails`, { email });
