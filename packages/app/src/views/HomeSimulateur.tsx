a816bd886e45d3507824c5dbeb88c76dc084db0d
freddy <freddy03h@gmail.com>
2019-06-20 12:08:00 +0200

    new design home simulator

--- packages/app/src/views/HomeSimulateur.tsx
+++ packages/app/src/views/HomeSimulateur.tsx
@@ -7,6 +7,7 @@ import globalStyles from "../utils/globalStyles";
 
 import Page from "../components/Page";
 import ActionLink from "../components/ActionLink";
+import ActionBar from "../components/ActionBar";
 import { ButtonSimulatorLink } from "../components/SimulatorLink";
 
 function HomeSimulateur(props: RouteComponentProps) {
@@ -27,22 +28,28 @@ function HomeSimulateur(props: RouteComponentProps) {
     navigator.clipboard.writeText(link);
   };
   return (
-    <Page
-      title="Bienvenue sur Index Egapro"
-      tagline="Afin de pouvoir réaccéder à tout moment à votre calcul : pensez à copier le code ci-dessus et le conserver précieusement."
-    >
+    <Page title="Bienvenue sur Index Egapro">
       <div css={styles.codeCopyBloc}>
-        <span ref={textEl} css={styles.codeCopyText} onClick={onCopy}>
-          {link}
-        </span>
+        <div css={styles.codeCopyFakeInput}>
+          <span ref={textEl} css={styles.codeCopyText} onClick={onCopy}>
+            {link}
+          </span>
+        </div>
         <ActionLink onClick={onCopy}>copier le lien</ActionLink>
       </div>
+
+      <p css={styles.tagline}>
+        Afin de pouvoir réaccéder à tout moment à votre calcul : pensez à copier
+        le code ci-dessus et le conserver précieusement.
+      </p>
+
       <div css={styles.imageContainer}>
         <div css={styles.image} />
       </div>
-      <div css={styles.action}>
+
+      <ActionBar>
         <ButtonSimulatorLink to="/effectifs" label="suivant" />
-      </div>
+      </ActionBar>
     </Page>
   );
 }
@@ -50,22 +57,31 @@ function HomeSimulateur(props: RouteComponentProps) {
 const styles = {
   codeCopyBloc: css({
     display: "flex",
-    alignItems: "flex-end",
-    marginBottom: 42
+    alignItems: "flex-end"
   }),
-  codeCopyText: css({
+  codeCopyFakeInput: css({
     flex: 1,
     marginRight: 20,
-    borderBottom: `solid ${globalStyles.colors.default} 1px`
-  }),
-  action: css({
+    height: 38,
     display: "flex",
-    flexDirection: "row",
-    marginTop: 32
+    alignItems: "center",
+    paddingLeft: globalStyles.grid.gutterWidth,
+    paddingRight: globalStyles.grid.gutterWidth,
+    background: "white",
+    border: `solid ${globalStyles.colors.default} 1px`
+  }),
+  codeCopyText: css({}),
+
+  tagline: css({
+    marginTop: 36,
+    fontSize: 14,
+    lineHeight: "17px"
   }),
 
   imageContainer: css({
-    height: 205 * 1.5,
+    marginTop: 160,
+    height: 151,
+    width: 384,
     position: "relative"
   }),
   image: css({
@@ -73,10 +89,10 @@ const styles = {
     right: 0,
     top: 0,
     bottom: 0,
-    left: -(globalStyles.grid.gutterWidth * 1.5),
+    left: -(globalStyles.grid.gutterWidth * 2),
     backgroundImage: `url(${
       process.env.PUBLIC_URL
-    }/illustration-simulator.svg)`,
+    }/illustration-home-simulator.svg)`,
     backgroundRepeat: "no-repeat",
     backgroundSize: "contain"
   })
