a4c9223b7c530d6def227bddb1ba54dcb720828e
freddy <freddy03h@gmail.com>
2019-07-18 12:10:25 +0200

    add react-piwik

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,16 +1,28 @@
 import React, { useReducer } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-
+import { Router } from "react-router-dom";
+import ReactPiwik from "react-piwik";
+import { createBrowserHistory } from "history";
 import AppReducer from "./AppReducer";
 
 import GridProvider from "./components/GridContext";
 
 import AppLayout from "./containers/AppLayout";
 
+const history = createBrowserHistory();
+
+const piwik = new ReactPiwik({
+  url: "stats.num.social.gouv.fr",
+  siteId: 11,
+  trackErrors: true
+});
+
+// track the initial pageview
+ReactPiwik.push(["trackPageView"]);
+
 function App() {
   const [state, dispatch] = useReducer(AppReducer, undefined);
   return (
-    <Router>
+    <Router history={piwik.connectToHistory(history)}>
       <GridProvider>
         <AppLayout state={state} dispatch={dispatch} />
       </GridProvider>
