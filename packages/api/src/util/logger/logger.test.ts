51aeed0a58ab5a8553584a76673d6e69abbd7908
Mathieu Agopian <mathieu@agopian.info>
2019-10-03 11:56:12 +0200

    Be a bit more lax on the logger tests

--- packages/api/src/util/logger/logger.test.ts
+++ packages/api/src/util/logger/logger.test.ts
@@ -1,37 +1,70 @@
 // @ts-ignore
 global.console = {
-    log: jest.fn(),
-    info: jest.fn(),
-    // error: jest.fn()
-    error: global.console.error
+  log: jest.fn(),
+  info: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn()
 };
 
 const someDate = new Date("2019-10-01 23:18:02");
 // @ts-ignore
 global.Date = class extends Date {
-    constructor() {
-        // @ts-ignore
-        return super(someDate);
-    }
-}
+  constructor() {
+    // @ts-ignore
+    return super(someDate);
+  }
+};
 
 describe("test the output of the logger", () => {
-    const logger = require("./");
-    test("info uses color escape sequences", () => {
-        logger.logger.info("just an info");
-        expect(global.console.log).toHaveBeenCalledWith("2019-10-01 23:18:02 \u001b[32minfo\u001b[39m: just an info ");
-    });
-    test("error uses color escape sequences and includes the stack trace", () => {
-        const error = new Error("this is an error");
-        logger.logger.error("just an error", error);
-        // @ts-ignore
-        // Grab the latest mock call, and get its first (and only) argument.
-        const latestMockCall = global.console.log.mock.calls.pop()[0];
-        // This argument starts with the timestamped colored message...
-        expect(latestMockCall.startsWith(
-            "2019-10-01 23:18:02 \u001b[31merror\u001b[39m: just an errorthis is an error\n"
-        )).toBeTruthy();
-        // ... and contains the stack trace.
-        expect(latestMockCall).toContain(error.stack);
-    });
+  const OLD_ENV = process.env;
+
+  beforeEach(() => {
+    jest.resetModules(); // this is important - it clears the cache
+    process.env = { ...OLD_ENV };
+  });
+
+  afterEach(() => {
+    process.env = OLD_ENV;
+  });
+
+  test("info uses console.log", () => {
+    const logger = require("./").logger;
+    logger.info("just an info");
+    // @ts-ignore
+    // Grab the latest mock call, and get its first (and only) argument.
+    const latestMockCall = global.console.log.mock.calls.pop()[0];
+    // Contains the timestamp.
+    expect(latestMockCall).toContain("2019-10-01 23:18:02");
+    // Contains the message.
+    expect(latestMockCall).toContain("just an info");
+  });
+  test("error uses console.log and includes the stack trace", () => {
+    const logger = require("./").logger;
+    const error = new Error("this is an error");
+    logger.error("just an error", error);
+    // @ts-ignore
+    // Grab the latest mock call, and get its first (and only) argument.
+    const latestMockCall = global.console.log.mock.calls.pop()[0];
+    // Contains the timestamp.
+    expect(latestMockCall).toContain("2019-10-01 23:18:02");
+    // Contains the message.
+    expect(latestMockCall).toContain("just an error");
+    // Contains the error.
+    expect(latestMockCall).toContain("this is an error");
+    // ... and contains the stack trace.
+    expect(latestMockCall).toContain(error.stack);
+  });
+  test("error sends a call to sentry if it's enabled", () => {
+    // Fake the enabling of sentry.
+    process.env.API_SENTRY_ENABLED = "true";
+    // Mock raven.captureException.
+    const raven = require("raven");
+    raven.captureException = jest.fn();
+    // Log an error.
+    const logger = require("./").logger;
+    const error = new Error("this is an error");
+    logger.error("just an error", error);
+    // Make sure the error was sent to sentry.
+    expect(raven.captureException).toHaveBeenCalledWith(error);
+  });
 });
