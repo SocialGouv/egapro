b546fdaeba9cb662cc87d452a1b9c34acae171a5
Mathieu Agopian <mathieu@agopian.info>
2019-10-04 20:08:33 +0200

    Simplify the env var managements for both local dev and docker deployment (#115)

--- packages/app/server.js
+++ packages/app/server.js
@@ -3,14 +3,15 @@ const path = require("path");
 const app = express();
 const proxy = require("http-proxy-middleware");
 
-const apiURL = process.env.API_URL;
+const apiURL = process.env.REACT_APP_API_URL;
 if (apiURL) {
+  console.log("[frontend] Proxying all calls to /api to ", apiURL);
   app.use("/api", proxy({ target: apiURL, changeOrigin: true }));
 }
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/*", function(req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
