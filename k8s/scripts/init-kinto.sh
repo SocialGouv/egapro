daa828862333627e3f18f0863a7e97b3a1422dd8
Nicolas Bovorasmy <nbovorasmy@octo.com>
2019-10-11 11:53:31 +0200

    ci: fix delete init kinto job

--- k8s/scripts/init-kinto.sh
+++ k8s/scripts/init-kinto.sh
@@ -7,7 +7,7 @@ if [ ! "$INIT_KINTO_POD_STATUS" ]
 then
     kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml
 else
-    kubectl delete job egapro-init-kinto"$1"
+    kubectl delete job egapro-init-kinto"$1" -n "$2"
     kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml
 fi
 
