2f589f44de7b455170e111f80f147cb29072a80d
Jean-Baptiste JOLY <jbjoly@octo.com>
2019-10-15 17:48:40 +0200

    ci: update kinto deployment in prod

--- k8s/scripts/init-kinto.sh
+++ k8s/scripts/init-kinto.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
-INIT_KINTO_POD_STATUS=$(kubectl get job egapro-init-kinto"$1" -n egapro"$1")
-
-# Check if egapro-init-kinto job exists
-if [ ! "$INIT_KINTO_POD_STATUS" ]
+if [ ${NAMESPACE} = "egapro-prod" ]
 then
-    kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml
-else
-    kubectl delete job egapro-init-kinto"$1" -n egapro"$1"
-    kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml
-fi
+    INIT_KINTO_POD_STATUS=$(kubectl get job egapro-init-kinto"$1" -n ${NAMESPACE})
 
+    # Check if egapro-init-kinto job exists
+    if [ ! "$INIT_KINTO_POD_STATUS" ]
+    then
+        kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml
+    else
+        kubectl delete job egapro-init-kinto"$1" -n ${NAMESPACE}
+        kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml
+    fi
+else
+    INIT_KINTO_POD_STATUS=$(kubectl get job egapro-init-kinto"$1" -n egapro"$1")
+
+    # Check if egapro-init-kinto job exists
+    if [ ! "$INIT_KINTO_POD_STATUS" ]
+    then
+        kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml
+    else
+        kubectl delete job egapro-init-kinto"$1" -n egapro"$1"
+        kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml
+    fi
+fi
