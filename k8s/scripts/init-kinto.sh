d595e9f09dcdbf740865b6b7b2f62619a70f4ec9
Jean-Baptiste JOLY <jbjoly@octo.com>
2019-10-17 16:31:07 +0200

    ci: user Git deploy image to send deployment URL to GitHub

--- k8s/scripts/init-kinto.sh
+++ k8s/scripts/init-kinto.sh
@@ -1,26 +1,12 @@
 #!/bin/sh
 
-if [ ${NAMESPACE} = "egapro-prod" ]
+INIT_KINTO_POD_STATUS=$(kubectl get job egapro-init-kinto"$1" -n egapro"$1")
+
+# Check if egapro-init-kinto job exists
+if [ ! "$INIT_KINTO_POD_STATUS" ]
 then
-    INIT_KINTO_POD_STATUS=$(kubectl get job egapro-init-kinto"$1" -n ${NAMESPACE})
-
-    # Check if egapro-init-kinto job exists
-    if [ ! "$INIT_KINTO_POD_STATUS" ]
-    then
-        kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml
-    else
-        kubectl delete job egapro-init-kinto"$1" -n ${NAMESPACE}
-        kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml
-    fi
+    kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml -n egapro"$1"
 else
-    INIT_KINTO_POD_STATUS=$(kubectl get job egapro-init-kinto"$1" -n egapro"$1")
-
-    # Check if egapro-init-kinto job exists
-    if [ ! "$INIT_KINTO_POD_STATUS" ]
-    then
-        kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml
-    else
-        kubectl delete job egapro-init-kinto"$1" -n egapro"$1"
-        kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml
-    fi
+    kubectl delete job egapro-init-kinto"$1" -n egapro"$1"
+    kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml -n egapro"$1"
 fi
