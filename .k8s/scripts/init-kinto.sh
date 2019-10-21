c04d89ddcf190033355b199eb650ddfc737599f5
Nicolas Bovorasmy <nbovorasmy@octo.com>
2019-10-21 23:16:20 +0200

    ci: change k8s directory to .k8s

--- .k8s/scripts/init-kinto.sh
+++ .k8s/scripts/init-kinto.sh
@@ -5,8 +5,8 @@ INIT_KINTO_POD_STATUS=$(kubectl get job egapro-init-kinto"$1" -n egapro"$1")
 # Check if egapro-init-kinto job exists
 if [ ! "$INIT_KINTO_POD_STATUS" ]
 then
-    kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml -n egapro"$1"
+    kubectl apply -f .k8s/kinto/job-init-kinto-egapro.yml -n egapro"$1"
 else
     kubectl delete job egapro-init-kinto"$1" -n egapro"$1"
-    kubectl apply -f k8s/kinto/job-init-kinto-egapro.yml -n egapro"$1"
+    kubectl apply -f .k8s/kinto/job-init-kinto-egapro.yml -n egapro"$1"
 fi
