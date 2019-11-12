05d425f31975a54418797f997d177c5d9f38720c
Mathieu Agopian <mathieu@agopian.info>
2019-11-12 16:59:39 +0100

    suppression de message de debug

--- packages/app/src/components/CellInput.tsx
+++ packages/app/src/components/CellInput.tsx
@@ -50,15 +50,11 @@ const trickyMaskNumberToAllowAnyChar = (
 
   const rawMaskArray = mask(valueOnlyNum);
 
-  console.log(rawMaskArray);
-
   const transformedMaskArray = rawMaskArray.map((char: any) => {
     return char.source && char.source === digitRegExp.source
       ? anyNonWhitespaceRegExp
       : char;
   });
-
-  console.log(transformedMaskArray);
 
   return transformedMaskArray;
 };
