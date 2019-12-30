da7ba56923d926a64b205e6f9d5dd7e22c6a7e3b
Nicolas Perriault <nicolas@perriault.net>
2019-12-30 16:42:39 +0100

    Amélioration de l'ajout du schéma à une collection existante.

--- packages/kinto/import-solen.py
+++ packages/kinto/import-solen.py
@@ -432,9 +432,6 @@ def initKintoClient(schema, truncate=False):
     try:
         coll = client.get_collection(id=KINTO_COLLECTION, bucket=KINTO_BUCKET)
         printer.success("La collection existe.")
-        if "schema" not in coll["data"]:
-            client.patch_collection(id=coll["data"]["id"], bucket=KINTO_BUCKET, changes={"schema": schema})
-            printer.info("Le schéma de validation JSON a été ajouté à la collection.")
     except KintoException as err:
         printer.warn("La collection n'existe pas, création")
         try:
@@ -442,6 +439,13 @@ def initKintoClient(schema, truncate=False):
             printer.success("La collection a été crée.")
         except KintoException as err:
             printer.error(f"Impossible de créer la collection: {err}")
+    if "schema" not in coll["data"]:
+        try:
+            client.patch_collection(id=coll["data"]["id"], bucket=KINTO_BUCKET, changes={"schema": schema})
+            printer.info("Le schéma de validation JSON a été ajouté à la collection.")
+        except KintoException as err:
+            printer.error("Impossible d'ajouter le schéma de validation à la collection {coll['data']['id']}.")
+            exit(1)
     return client
 
 
