a8ae5cfcd941911e64738a957c4981b3d8adad75
Mathieu Agopian <mathieu@agopian.info>
2020-02-14 13:54:15 +0100

    feat(declaration): ajout de l'iframe de désactivation de matomo dans la politique de confidentialité (#522)
    * feat(declaration): ajout de l'iframe de désactivation de matomo dans la politique de confidentialité
    
    * Ajout d'un title à l'iframe

--- packages/app/src/views/PolitiqueConfidentialite.tsx
+++ packages/app/src/views/PolitiqueConfidentialite.tsx
@@ -244,6 +244,14 @@ function CGU({ history, location, dispatch }: Props) {
           respectant les conditions d’exemption du consentement de l’internaute
           définies par la recommandation « Cookies » de la Commission nationale
           informatique et libertés (CNIL).
+          <br />
+          <br />
+          Vous pouvez tout de même désactiver cet outil :
+          <iframe
+            css={styles.iframe}
+            title="désactivez Matomo"
+            src="https://matomo.fabrique.social.gouv.fr/index.php?module=CoreAdminHome&action=optOut&language=fr&backgroundColor=&fontColor=&fontSize=&fontFamily="
+          ></iframe>
         </p>
         <h2 css={styles.title}>Mise à jour des conditions d’utilisation</h2>
         <p>
@@ -272,6 +280,9 @@ const styles = {
     thead: {
       backgroundColor: "#ccc"
     }
+  }),
+  iframe: css({
+    width: 600
   })
 };
 
