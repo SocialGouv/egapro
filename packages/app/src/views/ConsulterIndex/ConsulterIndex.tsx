70610c59139010c4059f535ecd4a7a938c20b668
Clément Berthou <clement.berthou@benextcompany.com>
2020-03-03 10:05:57 +0100

    feat: page de consultation de l'index

--- packages/app/src/views/ConsulterIndex/ConsulterIndex.tsx
+++ packages/app/src/views/ConsulterIndex/ConsulterIndex.tsx
@@ -1,12 +1,21 @@
+/** @jsx jsx */
 import * as React from "react";
-import {useEffect, useState} from "react";
-import {useDebounce, useInputValueChangeHandler} from "../../utils/hooks";
+import {useCallback, useState} from "react";
+import {css, jsx} from "@emotion/core";
+import {useInputValueChangeHandler} from "../../utils/hooks";
 import {findIndicatorsDataForRaisonSociale} from "../../utils/api";
 import {AppState} from "../../globals";
+import Field from "../../components/MinistereTravail/Field";
+import SearchButton from "../../components/MinistereTravail/SearchButton";
+import ConsulterIndexResult from "./ConsulterIndexResult";
+import SearchResultHeaderText from "./SearchResultHeaderText";
+import LogoIndex from "./LogoIndex";
+import {TEXT, TITLE} from "../../components/MinistereTravail/colors";
+import DownloadButton from "./DownloadButton";
+import SocialNetworksLinks from "./SocialNetworksLinks";
 
-const DEBOUNCE_DELAY = 500;
 
-interface FetchedIndicatorsData {
+export interface FetchedIndicatorsData {
   id: string,
   data: {
     informations: AppState["informations"];
@@ -16,63 +25,114 @@ interface FetchedIndicatorsData {
 }
 
 const ConsulterIndex: React.FC = () => {
-  const [raisonSociale, setRaisonSociale] = useState("test");
+  const [raisonSociale, setRaisonSociale] = useState("");
+  const [lastResearch, setLastResearch] = useState("");
   const [indicatorsData, setIndicatorsData] = useState([]);
 
   const handleRaisonSocialChange = useInputValueChangeHandler(setRaisonSociale);
-  const debouncedRaisonSociale = useDebounce(raisonSociale, DEBOUNCE_DELAY);
 
-  useEffect(() => {
-    if (debouncedRaisonSociale.length > 0) {
-      findIndicatorsDataForRaisonSociale(debouncedRaisonSociale)
+  const search = useCallback((event) => {
+    event.preventDefault();
+    if (raisonSociale.length > 0) {
+      setLastResearch(raisonSociale);
+      findIndicatorsDataForRaisonSociale(raisonSociale)
         .then(({ jsonBody }) => {
           setIndicatorsData(jsonBody);
         });
     }
-  }, [debouncedRaisonSociale, setIndicatorsData])
+  }, [raisonSociale, setIndicatorsData, setLastResearch]);
 
-  return (<div>
-    <div>
-      <label>Raison Sociale</label>
-      <input value={raisonSociale} placeholder="Raison Sociale" onChange={handleRaisonSocialChange}/>
+  return (<div css={styles.body}>
+    <div css={styles.logoWrapper}>
+      <LogoIndex />
     </div>
+    <h2 css={styles.title}>
+      Retrouvez l'Index égalité professionnelle F/H publié par les entreprises de plus de 1000 salariés.
+    </h2>
+    <p css={styles.subtitle}>
+      Dans une volonté de transparence et d'avancée sociale pour toutes et tous, le Ministère
+      du Travail a décidé de faciliter l'accès aux informations des entreprises qui publient leur
+      index depuis plus d'1 an.
+    </p>
+    <form css={styles.searchFieldWrapper} onSubmit={search}>
+      <Field value={raisonSociale} placeholder="Raison Sociale" onChange={handleRaisonSocialChange}/>
+      <SearchButton onClick={search}/>
+    </form>
 
-    <table>
-      <thead>
-        <tr>
-          <th>Structure</th>
-          <th>Raison Sociale</th>
-          <th>Nom UES</th>
-          <th>Région</th>
-          <th>Département</th>
-          <th>SIREN</th>
-          <th>Année de déclaration</th>
-          <th>Indice</th>
-        </tr>
-      </thead>
-      <tbody>
-        {indicatorsData.map(({
-          id,
-          data: {
-            informations,
-            informationsEntreprise,
-            declaration
-          }
-        }: FetchedIndicatorsData) => {
-          return (<tr key={id}>
-            <td>{informationsEntreprise.structure}</td>
-            <td>{informationsEntreprise.nomEntreprise}</td>
-            <td>{informationsEntreprise.nomUES}</td>
-            <td>{informationsEntreprise.region}</td>
-            <td>{informationsEntreprise.departement}</td>
-            <td>{informationsEntreprise.siren}</td>
-            <td>{informations.anneeDeclaration}</td>
-            <td>{declaration.totalPoint}/{declaration.totalPointCalculable}</td>
-          </tr>)
-        })}
-      </tbody>
-    </table>
+    {
+      lastResearch &&
+        <SearchResultHeaderText searchResults={indicatorsData} searchTerms={lastResearch} />
+    }
+    {
+      indicatorsData.length > 0 &&
+        <ConsulterIndexResult indicatorsData={indicatorsData}/>
+    }
+    <div css={styles.downloadSection}>
+      <div css={styles.downloadAlign}>
+        <div css={styles.downloadText}>Télécharger l'intégralité des données</div>
+        <div>
+          <DownloadButton/>
+        </div>
+      </div>
+    </div>
+    <div>
+      <p css={styles.subtitle}>
+        Porté par la loi « Pour la liberté de choisir son avenir personnel » du 5 septembre
+        2018, l'Index d'égalité professionnelle a été conçu pour faire progresser au sein
+        des entreprises l'égalité salariale entre les hommes et les femmes.
+      </p>
+    </div>
+    <div css={styles.socialNetworks}>
+      <SocialNetworksLinks />
+    </div>
   </div>);
+};
+
+const styles = {
+  logoWrapper: css({
+    marginBottom: "50px"
+  }),
+  searchFieldWrapper: css({
+    display: "flex",
+    width: "100%",
+    justifyContent: "center",
+    marginTop: "30px",
+    marginBottom: "30px",
+    fontSize: "18px"
+  }),
+  body: css({
+    backgroundColor: "white",
+    padding: "30px"
+  }),
+  title: css({
+    fontFamily: "Noto Sans",
+    fontWeight: "normal",
+    color: TITLE
+  }),
+  subtitle: css({
+    fontFamily: "Open Sans",
+    fontSize: "1em",
+    color: TEXT
+  }),
+  downloadSection: css({
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "20px",
+    marginBottom: "20px"
+  }),
+  downloadAlign: css({
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column"
+  }),
+  downloadText: css({
+    marginBottom: "10px",
+  }),
+  socialNetworks: css({
+    display: "flex",
+    justifyContent: "flex-end",
+    marginTop: "40px"
+  })
 };
 
 export default ConsulterIndex;
