0da7edd860c53895ac8ee0356730be46b3966ec6
Douglas Duteil <douglasduteil@gmail.com>
2020-04-01 00:04:23 +0200

    build(import-export): simplify dockerfile (#641)
    * fix(import-export): proper git checkout for a tag (ter)
    
    * build(import-export): simplify dockerfile
    
    * Add missing scripts and dependencies
    
    * Fix npm version
    
    * Update python to 3.8
    
    * Remove pipenv
    
    * build(docker): one copy per directory level
    
    * Revert to a debian-based image so "manylinux" wheels can be used (numpy, pandas, ...)
    
    * yarn.lock is already copied locally
    
    * missing -y
    
    * Keep curl for the import-export.sh script
    
    * Use python venv
    
    Co-authored-by: Mathieu Agopian <mathieu@agopian.info>

--- packages/import-export/import-export.sh
+++ packages/import-export/import-export.sh
@@ -34,6 +34,12 @@ echo ">>> RUNNING SCRIPT" `date`
 
 set -e
 
+echo ">>> INSTALLING PYTHON DEPENDENCIES"
+/app/venv/bin/pip install -r frozen-requirements.txt
+
+echo ">>> INSTALLING NODE DEPENDENCIES"
+yarn --frozen-lockfile
+
 echo ">>> DOWNLOAD 'LATEST' FILE CONTAINING LATEST DUMP NAME"
 az storage file download \
   --account-name $AZURE_STORAGE_ACCOUNT_NAME \
@@ -91,23 +97,17 @@ az storage file download \
   -p "DNUM - EXPORT SOLEN 2020.xlsx" \
   --dest "/tmp/solen_export_2020.xlsx"
 
-
-cd packages/import-export/
-
-echo ">>> INSTALLING PYTHON DEPENDENCIES"
-/app/venv/bin/pipenv install
-
 echo ">>> IMPORTING XLSX EXPORT FROM SOLEN: /tmp/solen_export_2019.xlsx"
-KINTO_SERVER=http://kinto:8888/v1 KINTO_COLLECTION=indicators_datas /app/venv/bin/pipenv run python solen.py /tmp/solen_export_2019.xlsx 2019 --progress
+KINTO_SERVER=http://kinto:8888/v1 KINTO_COLLECTION=indicators_datas /app/venv/bin/python solen.py /tmp/solen_export_2019.xlsx 2019 --progress
 
 echo ">>> IMPORTING XLSX EXPORT FROM SOLEN: /tmp/solen_export_2020.xlsx"
-KINTO_SERVER=http://kinto:8888/v1 KINTO_COLLECTION=indicators_datas /app/venv/bin/pipenv run python solen.py /tmp/solen_export_2020.xlsx 2020 --progress
+KINTO_SERVER=http://kinto:8888/v1 KINTO_COLLECTION=indicators_datas /app/venv/bin/python solen.py /tmp/solen_export_2020.xlsx 2020 --progress
 
 echo ">>> DUMPING DECLARATIONS TO /tmp/dump_declarations_records.json"
-KINTO_SERVER=http://kinto:8888/v1 /app/venv/bin/pipenv run python dump_records.py /tmp/dump_declarations_records.json
+KINTO_SERVER=http://kinto:8888/v1 /app/venv/bin/python dump_records.py /tmp/dump_declarations_records.json
 
 echo ">>> CONVERTING /tmp/dump_declarations_records.json TO /tmp/dump_declarations_records.xlsx"
-/app/venv/bin/pipenv run python json_to_xlsx.py /tmp/dump_declarations_records.json /tmp/dump_declarations_records.xlsx
+/app/venv/bin/python json_to_xlsx.py /tmp/dump_declarations_records.json /tmp/dump_declarations_records.xlsx
 
 
 echo ">>> UPLOADING /tmp/dump_declarations_records.json"
@@ -123,9 +123,6 @@ az storage file upload \
   --account-key $AZURE_STORAGE_ACCOUNT_KEY_EXPORT \
   --share-name "exports" \
   --source "/tmp/dump_declarations_records.xlsx"
-
-echo ">>> INSTALLING NODE DEPENDENCIES"
-/usr/bin/npm install
 
 echo ">>> INDEXING /tmp/dump_declarations_records.json in ElasticSearch"
 JSON_DUMP_FILENAME=/tmp/dump_declarations_records.json node index_elasticsearch.js
