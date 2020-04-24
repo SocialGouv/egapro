8fd808df9647033882c789950335c84acc9d1dc8
Mathieu Agopian <mathieu@agopian.info>
2020-04-24 16:32:37 +0200

    Filter to only migrate egapro records

--- packages/app/src/migrations/533-initial-migrations.ts
+++ packages/app/src/migrations/533-initial-migrations.ts
@@ -45,6 +45,7 @@ async function migrateData() {
   let { data, hasNextPage, next } = await collection.listRecords({
     filters: {
       "data.declaration.formValidated": "Valid",
+      "data.source": "egapro",
     },
   });
   let count = 0;
