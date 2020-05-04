df0fef87e4a38da5bde6cde0b4a446cae5433c2f
Mathieu Agopian <mathieu@agopian.info>
2020-05-04 09:39:00 +0200

    feat(migration): Migration des données dans le pipeline gitlab (#680)
    * feat(migration): Migration des données dans le pipeline gitlab
    
    * Try using a proper Dockerfile
    
    * Thanks Douglas <3
    
    * no before_script
    
    * Connect to the proper kinto server
    
    * authenticate to Kinto
    
    * Increase memory limit
    
    * Paginate the process so it consumes less RAM
    
    * add try/catch
    
    * Filter to only migrate egapro records
    
    * Better filter

--- packages/app/src/migrations/533-initial-migrations.ts
+++ packages/app/src/migrations/533-initial-migrations.ts
@@ -4,7 +4,7 @@ import fetch from "node-fetch";
 import totalNombreSalaries from "../utils/totalNombreSalaries";
 import calculIndicateurUn, {
   calculEcartTauxRemunerationParTrancheAgeCSP,
-  calculEcartTauxRemunerationParTrancheAgeCoef
+  calculEcartTauxRemunerationParTrancheAgeCoef,
 } from "../utils/calculsEgaProIndicateurUn";
 import calculIndicateurDeux from "../utils/calculsEgaProIndicateurDeux";
 import calculIndicateurTrois from "../utils/calculsEgaProIndicateurTrois";
@@ -14,59 +14,90 @@ import calculIndicateurQuatre from "../utils/calculsEgaProIndicateurQuatre";
 // @ts-ignore
 global.fetch = fetch;
 
-async function migrate() {
-  const [data, batchUpdate] = await getData();
-  console.log("Nombre d'enregistrements à migrer", data.length);
+const KINTO_URL = `http://${process.env.KINTO_SERVER}:8888/v1`;
 
-  await migrateTotalNombreSalaries(data);
-  await migrateEcartTauxRemunerationCSP(data);
-  await migrateEcartTauxRemunerationCoef(data);
-  await migrateNonCalculable(data);
-
-  console.log(">>> Sending the batch of updates");
-  const result = await batchUpdate((batch: any) => {
-    data.map((record: any) => batch.updateRecord(record));
-  });
+if (!process.env.KINTO_SERVER) {
+  console.log(
+    "Error: this script needs an env variable named KINTO_SERVER with the url to the kinto server, like 'localhost' or 'kinto-server'"
+  );
+  process.exit();
+} else {
+  console.log("Using the following kinto server URL:", KINTO_URL);
 }
 
-async function getData() {
-  const credentials = Buffer.from("admin:passw0rd").toString("base64");
-  const client = new KintoClient("http://localhost:8888/v1", {
+async function migrate() {
+  console.log(">>> Migrating the data");
+  const migratedRecords = await migrateData();
+  console.log(">>> Finished migrating", migratedRecords, "records");
+}
+
+async function migrateData() {
+  const credentials = Buffer.from(
+    `${process.env.KINTO_LOGIN}:${process.env.KINTO_PASSWORD}`
+  ).toString("base64");
+  const client = new KintoClient(KINTO_URL, {
     headers: {
-      Authorization: "Basic " + credentials
-    }
+      Authorization: "Basic " + credentials,
+    },
   });
   const collection = client.bucket("egapro").collection("indicators_datas");
 
   let { data, hasNextPage, next } = await collection.listRecords({
     filters: {
-      "data.declaration.formValidated": "Valid"
-    }
+      "data.declaration.formValidated": "Valid",
+      "exclude_data.source": "solen-2019,solen-2020",
+    },
   });
-  while (hasNextPage) {
+  let count = 0;
+  while (true) {
+    console.log(">>> Processing a page of data:", data.length, "records");
+
+    await migrateTotalNombreSalaries(data);
+    await migrateEcartTauxRemunerationCSP(data);
+    await migrateEcartTauxRemunerationCoef(data);
+    await migrateNonCalculable(data);
+
+    count += data.length;
+    console.log(">>> Sending a batch of updates:", data.length, "records");
+    await collection.batch((batch: any) => {
+      data.map((record: any) => batch.updateRecord(record));
+    });
+
+    if (!hasNextPage) {
+      break;
+    }
     const result = await next();
-    data = data.concat(result.data);
+    data = result.data;
     hasNextPage = result.hasNextPage;
   }
-  return [data, collection.batch.bind(collection)];
+  return count;
 }
 
 async function migrateTotalNombreSalaries(records: Array<any>) {
   console.log(">>> updating data.effectif.totalNombreSalaries");
   records.map((record: any) => {
-    const {
-      totalNombreSalariesHomme,
-      totalNombreSalariesFemme
-    } = totalNombreSalaries(record.data.effectif.nombreSalaries);
-    const total =
-      totalNombreSalariesHomme !== undefined &&
-      totalNombreSalariesFemme !== undefined
-        ? totalNombreSalariesHomme + totalNombreSalariesFemme
-        : undefined;
-    record.data = {
-      ...record.data,
-      effectif: { ...record.data.effectif, totalNombreSalaries: total }
-    };
+    try {
+      const {
+        totalNombreSalariesHomme,
+        totalNombreSalariesFemme,
+      } = totalNombreSalaries(record.data.effectif.nombreSalaries);
+      const total =
+        totalNombreSalariesHomme !== undefined &&
+        totalNombreSalariesFemme !== undefined
+          ? totalNombreSalariesHomme + totalNombreSalariesFemme
+          : undefined;
+      record.data = {
+        ...record.data,
+        effectif: { ...record.data.effectif, totalNombreSalaries: total },
+      };
+    } catch (error) {
+      console.log(
+        "error while processing record",
+        record.id,
+        ". Error:",
+        error
+      );
+    }
   });
   return records;
 }
@@ -77,13 +108,22 @@ async function migrateEcartTauxRemunerationCSP(records: Array<object>) {
   );
 
   records.map((record: any) => {
-    const remunerationAnnuelle = calculEcartTauxRemunerationParTrancheAgeCSP(
-      record.data.indicateurUn.remunerationAnnuelle
-    );
-    record.data = {
-      ...record.data,
-      indicateurUn: { ...record.data.indicateurUn, remunerationAnnuelle }
-    };
+    try {
+      const remunerationAnnuelle = calculEcartTauxRemunerationParTrancheAgeCSP(
+        record.data.indicateurUn.remunerationAnnuelle
+      );
+      record.data = {
+        ...record.data,
+        indicateurUn: { ...record.data.indicateurUn, remunerationAnnuelle },
+      };
+    } catch (error) {
+      console.log(
+        "error while processing record",
+        record.id,
+        ". Error:",
+        error
+      );
+    }
   });
   return records;
 }
@@ -94,13 +134,22 @@ async function migrateEcartTauxRemunerationCoef(records: Array<object>) {
   );
 
   records.map((record: any) => {
-    const coefficient = calculEcartTauxRemunerationParTrancheAgeCoef(
-      record.data.indicateurUn.coefficient
-    );
-    record.data = {
-      ...record.data,
-      indicateurUn: { ...record.data.indicateurUn, coefficient }
-    };
+    try {
+      const coefficient = calculEcartTauxRemunerationParTrancheAgeCoef(
+        record.data.indicateurUn.coefficient
+      );
+      record.data = {
+        ...record.data,
+        indicateurUn: { ...record.data.indicateurUn, coefficient },
+      };
+    } catch (error) {
+      console.log(
+        "error while processing record",
+        record.id,
+        ". Error:",
+        error
+      );
+    }
   });
   return records;
 }
@@ -109,44 +158,53 @@ async function migrateNonCalculable(records: Array<object>) {
   console.log(">>> updating data.indicateurX.nonCalculable");
 
   records.map((record: any) => {
-    const {
-      effectifsIndicateurCalculable: indicateurUnCalculable
-    } = calculIndicateurUn(record.data);
-    const {
-      effectifsIndicateurCalculable: indicateurDeuxCalculable
-    } = calculIndicateurDeux(record.data);
-    const {
-      effectifsIndicateurCalculable: indicateurTroisCalculable
-    } = calculIndicateurTrois(record.data);
-    const {
-      effectifsIndicateurCalculable: indicateurDeuxTroisCalculable
-    } = calculIndicateurDeuxTrois(record.data);
-    const {
-      indicateurCalculable: indicateurQuatreCalculable
-    } = calculIndicateurQuatre(record.data);
-    record.data = {
-      ...record.data,
-      indicateurUn: {
-        ...record.data.indicateurUn,
-        nonCalculable: !indicateurUnCalculable
-      },
-      indicateurDeux: {
-        ...record.data.indicateurDeux,
-        nonCalculable: !indicateurDeuxCalculable
-      },
-      indicateurTrois: {
-        ...record.data.indicateurTrois,
-        nonCalculable: !indicateurTroisCalculable
-      },
-      indicateurDeuxTrois: {
-        ...record.data.indicateurDeuxTrois,
-        nonCalculable: !indicateurDeuxTroisCalculable
-      },
-      indicateurQuatre: {
-        ...record.data.indicateurQuatre,
-        nonCalculable: !indicateurQuatreCalculable
-      }
-    };
+    try {
+      const {
+        effectifsIndicateurCalculable: indicateurUnCalculable,
+      } = calculIndicateurUn(record.data);
+      const {
+        effectifsIndicateurCalculable: indicateurDeuxCalculable,
+      } = calculIndicateurDeux(record.data);
+      const {
+        effectifsIndicateurCalculable: indicateurTroisCalculable,
+      } = calculIndicateurTrois(record.data);
+      const {
+        effectifsIndicateurCalculable: indicateurDeuxTroisCalculable,
+      } = calculIndicateurDeuxTrois(record.data);
+      const {
+        indicateurCalculable: indicateurQuatreCalculable,
+      } = calculIndicateurQuatre(record.data);
+      record.data = {
+        ...record.data,
+        indicateurUn: {
+          ...record.data.indicateurUn,
+          nonCalculable: !indicateurUnCalculable,
+        },
+        indicateurDeux: {
+          ...record.data.indicateurDeux,
+          nonCalculable: !indicateurDeuxCalculable,
+        },
+        indicateurTrois: {
+          ...record.data.indicateurTrois,
+          nonCalculable: !indicateurTroisCalculable,
+        },
+        indicateurDeuxTrois: {
+          ...record.data.indicateurDeuxTrois,
+          nonCalculable: !indicateurDeuxTroisCalculable,
+        },
+        indicateurQuatre: {
+          ...record.data.indicateurQuatre,
+          nonCalculable: !indicateurQuatreCalculable,
+        },
+      };
+    } catch (error) {
+      console.log(
+        "error while processing record",
+        record.id,
+        ". Error:",
+        error
+      );
+    }
   });
   return records;
 }
