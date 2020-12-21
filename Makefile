4549b9e68915fcf6dc04cdc6305a933b56ec4f23
Mathieu Agopian <mathieu@agopian.info>
2020-12-21 14:54:03 +0100

    Publishing

--- Makefile
+++
@@ -1,5 +0,0 @@
-clean:
-	rm -rf dist/
-dist: clean
-	yarn run build
-	mv build dist
--- src/__fixtures__/stateDefault.tsx
+++
@@ -1,5 +0,0 @@
-import AppReducer from "../AppReducer";
-
-const stateDefault = AppReducer(undefined, { type: "initiateState", data: {} });
-
-export default stateDefault;
