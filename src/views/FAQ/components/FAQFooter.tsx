ffe5ce0d1a64dce04b2d61a56a0deaea850a9227
Mathieu Agopian <mathieu@agopian.info>
2020-12-24 14:34:16 +0100

    No more hotline

--- src/views/FAQ/components/FAQFooter.tsx
+++ src/views/FAQ/components/FAQFooter.tsx
@@ -20,17 +20,6 @@ function FAQFooter() {
             Contactez votre référent Egapro
           </a>
         </span>
-        <br />
-        <span css={styles.text}>
-          ou appelez Allô Index Egapro
-          <a
-            href="tel:+33800009110"
-            css={styles.alloEgapro}
-            title="Allô Index Egapro : 0 800 009 110 (Service gratuit + prix appel)"
-          >
-            0 800 009 110
-          </a>
-        </span>
       </div>
     </div>
   );
@@ -44,30 +33,16 @@ const styles = {
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    borderTop: "1px solid #EFECEF"
+    borderTop: "1px solid #EFECEF",
   }),
   text: css({
     fontSize: 12,
-    lineHeight: "15px"
+    lineHeight: "15px",
   }),
   infoLink: {
     color: globalStyles.colors.default,
-    textDecoration: "underline"
+    textDecoration: "underline",
   },
-  alloEgapro: {
-    backgroundImage: `url(${AlloEgaproBackgroundImage})`,
-    backgroundSize: "contain",
-    backgroundRepeat: "no-repeat",
-    color: "#91919b",
-    display: "inline-block",
-    fontFamily: "arial",
-    fontWeight: "bold" as "bold",
-    fontSize: 20,
-    marginTop: 10,
-    padding: "10px 50px 10px 13px",
-    textDecoration: "none",
-    width: "100%"
-  }
 };
 
 export default FAQFooter;
