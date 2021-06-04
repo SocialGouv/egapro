ee38263ee353815abd8dc7766c9700299d2b334a
Mathieu Agopian <mathieu@agopian.info>
2021-06-04 16:25:34 +0200

    Relax the height requirement for cells to improve spacing

--- src/views/Recapitulatif/components/RowData.tsx
+++ src/views/Recapitulatif/components/RowData.tsx
@@ -12,7 +12,7 @@ function RowData({
   name,
   data,
   asPercent,
-  message
+  message,
 }: {
   name: string;
   data: Array<number | undefined>;
@@ -32,7 +32,7 @@ function RowData({
               datum !== undefined && Math.sign(datum) > 0
                 ? styles.cellMen
                 : styles.cellWomen,
-              datum === undefined && styles.cellEmpty
+              datum === undefined && styles.cellEmpty,
             ]}
           >
             {datum !== undefined
@@ -51,7 +51,7 @@ function RowData({
 export function RowDataFull({
   name,
   data,
-  asPercent
+  asPercent,
 }: {
   name: string;
   data: number | undefined;
@@ -69,7 +69,7 @@ export function RowDataFull({
             data !== undefined && Math.sign(data) > 0
               ? styles.cellMen
               : styles.cellWomen,
-            data === undefined && styles.cellEmpty
+            data === undefined && styles.cellEmpty,
           ]}
         >
           {data !== undefined
@@ -115,55 +115,53 @@ const styles = {
     flexDirection: "column",
     alignItems: "stretch",
     height: 51,
-    marginBottom: 10
+    marginBottom: 10,
   }),
   row: css({
     display: "flex",
     flexDirection: "row",
-    alignItems: "flex-start"
+    alignItems: "flex-start",
   }),
   rowLabel: css({
-    marginBottom: 10
+    marginBottom: 10,
   }),
   cellHead: css({
-    height: 22,
     paddingBottom: 2,
     display: "flex",
     alignItems: "flex-end",
     borderBottom: `solid ${globalStyles.colors.default} 1px`,
-    fontSize: 14
+    fontSize: 14,
   }),
   cellHeadInvalid: css({
     color: globalStyles.colors.invalid,
-    borderColor: "transparent"
+    borderColor: "transparent",
   }),
 
   cell: css({
     marginLeft: 4,
     width: 48,
-    height: 22,
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-    fontSize: 14
+    fontSize: 14,
   }),
   cellMen: css({
-    color: globalStyles.colors.men
+    color: globalStyles.colors.men,
   }),
   cellWomen: css({
-    color: globalStyles.colors.women
+    color: globalStyles.colors.women,
   }),
   cellEmpty: css({
-    color: globalStyles.colors.invalid
+    color: globalStyles.colors.invalid,
   }),
   cellLabel: css({
     fontSize: 12,
     fontStyle: "italic",
-    textAlign: "center"
+    textAlign: "center",
   }),
   cellFull: css({
-    width: 48 * 4 + 3 * 4
-  })
+    width: 48 * 4 + 3 * 4,
+  }),
 };
 
 export default RowData;
