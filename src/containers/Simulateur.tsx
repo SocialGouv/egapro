2ed2d3a466fa07bf8207f3f21dc340996776a983
pom421 <mauguet.po@gmail.com>
2021-10-08 15:14:08 +0200

    fix: remove warning in react useEffect

--- src/containers/Simulateur.tsx
+++ src/containers/Simulateur.tsx
@@ -90,7 +90,7 @@ function Simulateur({ code, state, dispatch }: Props) {
         setLoading(false);
         setTokenInfo("error");
       });
-  }, [token]);
+  }, [token, dispatch]);
 
   useEffect(() => {
     setLoading(true);
