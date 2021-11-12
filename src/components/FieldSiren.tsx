021a51b1eb1b4dabb92458afd81cbfe40ea4cdac
pom421 <mauguet.po@gmail.com>
2021-11-12 12:52:27 +0100

    fix: typo in previous conflict resolution

--- src/components/FieldSiren.tsx
+++ src/components/FieldSiren.tsx
@@ -5,8 +5,6 @@ import Input, { hasFieldError } from "./Input"
 
 import globalStyles from "../utils/globalStyles"
 import { isEmpty } from "../utils/object"
-
-  ValidatorFunction,
 import { composeValidators, required, simpleMemoize, ValidatorFunction } from "../utils/formHelpers"
 import { ownersForSiren, validateSiren } from "../utils/api"
 import { entrepriseData } from "../views/InformationsEntreprise/InformationsEntrepriseForm"
