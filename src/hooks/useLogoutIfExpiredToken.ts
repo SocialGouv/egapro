6c178dd414d343ffc1bb377e84f1e5fd4b4102ee
pom421 <mauguet.po@gmail.com>
2021-11-30 17:25:10 +0100

    fix: needs a useEffect to change the context

--- src/hooks/useLogoutIfExpiredToken.ts
+++ src/hooks/useLogoutIfExpiredToken.ts
@@ -1,10 +1,13 @@
+import React from "react"
 import { useUser } from "../components/AuthContext"
 import { EXPIRED_TOKEN_MESSAGE } from "../utils/fetcher"
 
 export function useLogoutIfExpiredToken(error: Error & { info: string; status: number }) {
   const { logout } = useUser()
 
-  if (error?.info === EXPIRED_TOKEN_MESSAGE) {
-    logout()
-  }
+  React.useEffect(() => {
+    if (error?.info === EXPIRED_TOKEN_MESSAGE) {
+      logout()
+    }
+  }, [error])
 }
