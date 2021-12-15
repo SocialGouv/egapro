a85532a14040fa7310d33c58c9045ab8ce019341
pom421 <mauguet.po@gmail.com>
2021-12-15 15:41:31 +0100

    feat: add 2 calls to ensure have data siren

--- src/hooks/useSiren.ts
+++ src/hooks/useSiren.ts
@@ -4,7 +4,29 @@ import { makeMessage } from "../utils/makeMessage"
 import { useLogoutIfExpiredToken } from "./useLogoutIfExpiredToken"
 
 export function useSiren(siren: string) {
-  const { data: entreprise, error, mutate } = useSWR(siren ? `/validate-siren?siren=${siren}` : null, fetcher)
+  let entreprise, error, mutate
+
+  const result = useSWR(siren ? `/entreprise/${siren}` : null, fetcher)
+
+  if (result?.data) {
+    entreprise = result.data
+    error = result.error
+    mutate = result.mutate
+  }
+  useLogoutIfExpiredToken(error)
+
+  // Si l'entreprise n'existe pas chez nous, on essaye via l'API Entreprise.
+  const resultFromApiEntreprise = useSWR(
+    siren && result && result.error ? `/validate-siren?siren=${siren}` : null,
+    fetcher,
+  )
+
+  if (resultFromApiEntreprise?.data) {
+    entreprise = resultFromApiEntreprise.data
+    error = resultFromApiEntreprise.error
+    mutate = resultFromApiEntreprise.mutate
+  }
+
   useLogoutIfExpiredToken(error)
 
   const isLoading = !entreprise && !error
