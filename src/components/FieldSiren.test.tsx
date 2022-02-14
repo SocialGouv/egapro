cf876af616ad2accb3a1fe08974d73b86950a9e1
pom421 <mauguet.po@gmail.com>
2022-02-14 12:25:05 +0100

    chore: typo

--- src/components/FieldSiren.test.tsx
+++ src/components/FieldSiren.test.tsx
@@ -83,7 +83,7 @@ describe("isValidSiren", () => {
     )
   })
 
-  test("returns an error message for not allowed user", async () => {
+  test("returns an error message for a not allowed user", async () => {
     await expect(validator("234567891")).resolves.toBe("Vous n'êtes pas autorisé à déclarer pour ce SIREN.")
   })
 })
