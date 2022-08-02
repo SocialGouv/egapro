b892959cca7af30d2511b7c7be559e5edfbbabc9
pom421 <mauguet.po@gmail.com>
2022-08-02 09:45:15 +0200

    feat: wording
    Fixes #884.

--- src/components/ds/NoSiren.tsx
+++ src/components/ds/NoSiren.tsx
@@ -1,24 +1,30 @@
-import { chakra, Text } from "@chakra-ui/react"
+import { Text } from "@chakra-ui/react"
 import * as React from "react"
-import TextLink from "./TextLink"
+import { Link } from "@chakra-ui/react"
+
+import { useUser } from "../AuthContext"
+import { IconExternalLink } from "./Icons"
 
 const NoSiren: React.FC = () => {
+  const { email } = useUser()
+
   return (
     <>
       <Text>Votre email n'est rattaché à aucun Siren.</Text>
       <Text mt="4">
-        Vous pouvez faire une demande de rattachement en cliquant{" "}
-        <TextLink to="mailto:dgt.ega-pro@travail.gouv.fr" isExternal>
-          ici
-        </TextLink>
+        Vous pouvez faire une demande rattachement en indiquant votre SIREN et email en cliquant{" "}
+        <Link
+          isExternal
+          textDecoration="underline"
+          href={`mailto:dgt.ega-pro@travail.gouv.fr?subject=EgaPro - Demander à être déclarant d'un SIREN&body=Bonjour, je souhaite être déclarant pour le SIREN >>indiquer votre SIREN<<. Mon email de déclaration est ${email}. Cordialement.`}
+        >
+          ici&nbsp;
+          <IconExternalLink sx={{ transform: "translateY(.125rem)" }} />
+        </Link>
         .
       </Text>
       <Text mt="4">
-        Si ce lien ne fonctionne pas, vous pouvez nous envoyer votre Siren et email à{" "}
-        <chakra.span bgColor="orange.100" px="2" py="1" borderRadius="4" fontWeight="semibold">
-          dgt.ega-pro@travail.gouv.fr
-        </chakra.span>
-        .
+        Si ce lien ne fonctionne pas, vous pouvez nous envoyer votre SIREN et email à dgt.ega-pro@travail.gouv.fr .
       </Text>
     </>
   )
