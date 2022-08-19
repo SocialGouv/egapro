d9ba3114f4f02f0f8ca08bdfb33ec8e27b2bdbb1
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2022-08-19 14:29:22 +0200

    Update views.py

--- packages/api/egapro/views.py
+++ packages/api/egapro/views.py
@@ -189,30 +189,6 @@ async def get_declarations(request, response, siren):
         raise HttpError(404, f"No declarations with siren {siren} for any year")
     response.json = declarations
 
-@tokens.require
-@app.route("/declarations/{siren}", methods=["GET"])
-async def get_declarations(request, response, siren):
-    declarations = []
-    limit = request.query.int("limit", 10)
-    years = sorted(constants.YEARS, reverse=True)
-
-    for year in years:
-        try:
-            record = await db.declaration.get(siren, year)
-
-            resource = record.as_resource()
-            if record.data.path("déclarant.nom"):
-                await helpers.patch_from_recherche_entreprises(resource["data"])
-            declarations.append(resource)
-        except:
-            pass
-        if len(declarations) == limit:
-            break
-
-    if not declarations:
-        raise HttpError(404, f"No declarations with siren {siren} for any year")
-    response.json = declarations
-
 
 @app.route("/declaration/{siren}/{year}", methods=["GET"])
 @tokens.require
