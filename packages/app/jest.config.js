86a6df6a0ef8d88a51ae919b63a67d5b6588d390
Laurent Sutterlity <laurent@sutterlity.studio>
2022-09-22 14:28:27 +0200

    fix: lint js and jsx files

--- packages/app/jest.config.js
+++ packages/app/jest.config.js
@@ -1,9 +1,9 @@
-const nextJest = require("next/jest")
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
-})
+});
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
@@ -27,9 +27,9 @@ const customJestConfig = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
     "^@common/(.*)$": "<rootDir>/src/common/$1",
-    "^@components/(.*)$": "<rootDir>/src/components/$1"
+    "^@components/(.*)$": "<rootDir>/src/components/$1",
   },
-}
+};
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
