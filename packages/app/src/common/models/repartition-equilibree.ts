edf2298eaf58988d6466f651d08abd1d751ad1e1
pom421 <mauguet.po@gmail.com>
2022-09-22 15:54:09 +0200

    refactor: format ts file

--- packages/app/src/common/models/repartition-equilibree.ts
+++ packages/app/src/common/models/repartition-equilibree.ts
@@ -1,57 +1,57 @@
-import { COUNTIES, COUNTRIES, NAF, REGIONS } from "../dict"
+import type { COUNTIES, COUNTRIES, NAF, REGIONS } from "@common/dict";
 
 export type RepartitionEquilibreeAPI = {
-  siren: string
-  year: number
-  data: RepartitionEquilibreeDataField
-  modified_at: number
-  declared_at: number
-}
+  data: RepartitionEquilibreeDataField;
+  declared_at: number;
+  modified_at: number;
+  siren: string;
+  year: number;
+};
 
 type Declarant = {
-  prénom: string
-  nom: string
-  téléphone: string
-  email: string
-}
+  email: string;
+  nom: string;
+  prénom: string;
+  téléphone: string;
+};
 
 type Entreprise = {
-  siren: string
-  raison_sociale: string
-  code_naf: keyof typeof NAF
-  région: keyof typeof REGIONS
-  département: keyof typeof COUNTIES
-  adresse: string
-  code_postal?: string | undefined
-  commune: string
-  code_pays?: keyof typeof COUNTRIES | undefined
-}
+  adresse: string;
+  code_naf: keyof typeof NAF;
+  code_pays?: keyof typeof COUNTRIES | undefined;
+  code_postal?: string | undefined;
+  commune: string;
+  département: keyof typeof COUNTIES;
+  raison_sociale: string;
+  région: keyof typeof REGIONS;
+  siren: string;
+};
 
 export type RepartitionEquilibreeDataField = {
-  source: "répartition_équilibrée"
-  déclarant: Declarant
-  entreprise: Entreprise
-  déclaration: DeclarationRepartitionEquilibree
-  répartition_équilibrée: IndicateursRepartitionEquilibree
-}
+  déclarant: Declarant;
+  déclaration: DeclarationRepartitionEquilibree;
+  entreprise: Entreprise;
+  répartition_équilibrée: IndicateursRepartitionEquilibree;
+  source: "répartition_équilibrée";
+};
 
 type DeclarationRepartitionEquilibree = {
-  année_indicateurs: number
-  fin_période_référence: Date
-  publication: PublicationRepartitionEquilibree
-}
+  année_indicateurs: number;
+  fin_période_référence: Date;
+  publication: PublicationRepartitionEquilibree;
+};
 
 type PublicationRepartitionEquilibree = {
-  date: string
-  url?: string | undefined
-  modalités?: string | undefined
-}
+  date: string;
+  modalités?: string | undefined;
+  url?: string | undefined;
+};
 
 type IndicateursRepartitionEquilibree = {
-  pourcentage_femmes_cadres: number
-  pourcentage_hommes_cadres: number
-  pourcentage_femmes_membres: number
-  pourcentage_hommes_membres: number
-  motif_non_calculabilité_cadres: "aucun_cadre_dirigeant" | "un_seul_cadre_dirigeant" | undefined
-  motif_non_calculabilité_membres: "aucune_instance_dirigeante" | undefined
-}
+  motif_non_calculabilité_cadres: "aucun_cadre_dirigeant" | "un_seul_cadre_dirigeant" | undefined;
+  motif_non_calculabilité_membres: "aucune_instance_dirigeante" | undefined;
+  pourcentage_femmes_cadres: number;
+  pourcentage_femmes_membres: number;
+  pourcentage_hommes_cadres: number;
+  pourcentage_hommes_membres: number;
+};
