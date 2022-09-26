04b96d55e7dbe52f54f5e700d38860b4903f3e7b
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2022-09-26 18:14:49 +0200

    fix: restore fetch all years functionality

--- packages/simulateur/src/hooks/useDeclaration.ts
+++ packages/simulateur/src/hooks/useDeclaration.ts
@@ -49,29 +49,73 @@ export function useDeclaration(
 }
 
 export function useDeclarations(siren: string): FetcherReturn & { declarations: Record<string, DeclarationAPI> } {
+
+  const normalizedSiren = siren && siren.length === 9 ? siren : undefined
+
+  const { data, error, mutate } = useSWR<DeclarationAPI[]>(
+    normalizedSiren ? `/declarations/${normalizedSiren}` : null,
+    fetcher,
+    {
+      onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+        // Never retry on 404.
+        if (error.status === 404) return
+
+        // Never retry on 403 (Forbidden).
+        if (error.status === 403) return
+
+        // Never retry on 422 (Unprocessable Entity). SIREN seen invalid by our API.
+        if (error.status === 422) return
+
+        // Only retry up to 3 times.
+        if (retryCount >= 3) return
+
+        // Retry after 5 seconds.
+        setTimeout(() => revalidate({ retryCount }), 5000)
+      },
+    },
+  )
+
+  const isLoading = !data && !error
+  const isError = Boolean(error)
+
+  const dataWithoutDrafts = data?.filter((declaration) => Boolean(declaration.declared_at))
+
+  const declarations = !dataWithoutDrafts
+    ? {}
+    : dataWithoutDrafts.reduce((acc, declaration) => ({ ...acc, [String(declaration.year)]: declaration }), {})
+
+  return {
+    declarations,
+    error,
+    message: genericErrorMessage(error),
+    isLoading,
+    isError,
+    mutate,
+  }
+
   // Naive way to fetch the last 3 declarations. Waiting for the API to have an endpoint to retrieve all declarations for a SIREN.
-  const { declaration: declaration2021, error: error2021, isLoading: isLoading2021 } = useDeclaration(siren, 2021)
-  const { declaration: declaration2020, error: error2020, isLoading: isLoading2020 } = useDeclaration(siren, 2020)
-  const { declaration: declaration2019, error: error2019, isLoading: isLoading2019 } = useDeclaration(siren, 2019)
+  // const { declaration: declaration2021, error: error2021, isLoading: isLoading2021 } = useDeclaration(siren, 2021)
+  // const { declaration: declaration2020, error: error2020, isLoading: isLoading2020 } = useDeclaration(siren, 2020)
+  // const { declaration: declaration2019, error: error2019, isLoading: isLoading2019 } = useDeclaration(siren, 2019)
 
-  if (error2021) console.debug("Pas de déclaration en 2021 ou erreur")
-  if (error2020) console.debug("Pas de déclaration en 2020 ou erreur")
-  if (error2019) console.debug("Pas de déclaration en 2019 ou erreur")
+  // if (error2021) console.debug("Pas de déclaration en 2021 ou erreur")
+  // if (error2020) console.debug("Pas de déclaration en 2020 ou erreur")
+  // if (error2019) console.debug("Pas de déclaration en 2019 ou erreur")
 
-  const isLoading = isLoading2021 && isLoading2020 && isLoading2019
+  // const isLoading = isLoading2021 && isLoading2020 && isLoading2019
 
   // data.déclaration.brouillon == true uniquement pour les déclarations faites via la déclaration directe et non finalisées.
-  return {
-    declarations: {
-      ...(declaration2021 && declaration2021.data.déclaration.brouillon !== true && { 2021: declaration2021 }),
-      ...(declaration2020 && declaration2020.data.déclaration.brouillon !== true && { 2020: declaration2020 }),
-      ...(declaration2019 && declaration2019.data.déclaration.brouillon !== true && { 2019: declaration2019 }),
-    },
-    isLoading,
-    error: null,
-    isError: false,
-    message: null,
-    // eslint-disable-next-line
-    mutate: () => {},
-  }
+  // return {
+  //   declarations: {
+  //     ...(declaration2021 && declaration2021.data.déclaration.brouillon !== true && { 2021: declaration2021 }),
+  //     ...(declaration2020 && declaration2020.data.déclaration.brouillon !== true && { 2020: declaration2020 }),
+  //     ...(declaration2019 && declaration2019.data.déclaration.brouillon !== true && { 2019: declaration2019 }),
+  //   },
+  //   isLoading,
+  //   error: null,
+  //   isError: false,
+  //   message: null,
+  //   // eslint-disable-next-line
+  //   mutate: () => {},
+  // }
 }
