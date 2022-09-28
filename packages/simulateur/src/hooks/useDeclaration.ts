87269d6b7e50df90eda160ac99e0edb49650e35f
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2022-09-28 11:21:18 +0200

    remove comments

--- packages/simulateur/src/hooks/useDeclaration.ts
+++ packages/simulateur/src/hooks/useDeclaration.ts
@@ -49,7 +49,6 @@ export function useDeclaration(
 }
 
 export function useDeclarations(siren: string): FetcherReturn & { declarations: Record<string, DeclarationAPI> } {
-
   const normalizedSiren = siren && siren.length === 9 ? siren : undefined
 
   const { data, error, mutate } = useSWR<DeclarationAPI[]>(
@@ -92,30 +91,4 @@ export function useDeclarations(siren: string): FetcherReturn & { declarations: 
     isError,
     mutate,
   }
-
-  // Naive way to fetch the last 3 declarations. Waiting for the API to have an endpoint to retrieve all declarations for a SIREN.
-  // const { declaration: declaration2021, error: error2021, isLoading: isLoading2021 } = useDeclaration(siren, 2021)
-  // const { declaration: declaration2020, error: error2020, isLoading: isLoading2020 } = useDeclaration(siren, 2020)
-  // const { declaration: declaration2019, error: error2019, isLoading: isLoading2019 } = useDeclaration(siren, 2019)
-
-  // if (error2021) console.debug("Pas de déclaration en 2021 ou erreur")
-  // if (error2020) console.debug("Pas de déclaration en 2020 ou erreur")
-  // if (error2019) console.debug("Pas de déclaration en 2019 ou erreur")
-
-  // const isLoading = isLoading2021 && isLoading2020 && isLoading2019
-
-  // data.déclaration.brouillon == true uniquement pour les déclarations faites via la déclaration directe et non finalisées.
-  // return {
-  //   declarations: {
-  //     ...(declaration2021 && declaration2021.data.déclaration.brouillon !== true && { 2021: declaration2021 }),
-  //     ...(declaration2020 && declaration2020.data.déclaration.brouillon !== true && { 2020: declaration2020 }),
-  //     ...(declaration2019 && declaration2019.data.déclaration.brouillon !== true && { 2019: declaration2019 }),
-  //   },
-  //   isLoading,
-  //   error: null,
-  //   isError: false,
-  //   message: null,
-  //   // eslint-disable-next-line
-  //   mutate: () => {},
-  // }
 }
