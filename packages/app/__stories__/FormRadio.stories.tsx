0be35c6603ae059ffa74ab8c233ff10b321c2d3d
Laurent Sutterlity <laurent@sutterlity.studio>
2022-09-30 10:42:41 +0200

    refactor: refactoring FormRadio

--- packages/app/__stories__/FormRadio.stories.tsx
+++ packages/app/__stories__/FormRadio.stories.tsx
@@ -22,57 +22,45 @@ const Template: ComponentStory<typeof FormRadioGroup> = args => (
   </FormRadioGroup>
 );
 
-export const Default = Template.bind({});
+export const Default = Template.bind({ size: "md" });
 
-export const Small: ComponentStory<typeof FormRadioGroup> = args => {
+export const SizeSm = Template.bind({});
+SizeSm.args = {
+  ...Default.args,
+  size: "sm",
+  inline: false,
+};
+
+export const Inline = Template.bind({});
+Inline.args = {
+  ...Default.args,
+  inline: true,
+};
+
+export const IsValid: ComponentStory<typeof FormRadioGroup> = args => {
   return (
-    <FormRadioGroup {...args}>
+    <FormRadioGroup isValid ariaLabelledby="legendId xxxx" {...args}>
       <FormRadioGroup.Legend id="legendId">Légende</FormRadioGroup.Legend>
-      <FormRadioGroup.Input id="inputId1" size="sm">
-        radio 1
-      </FormRadioGroup.Input>
-      <FormRadioGroup.Input id="inputId2" size="sm">
-        radio 2
-      </FormRadioGroup.Input>
-      <FormRadioGroup.Input id="inputId3" size="sm">
-        radio 3
-      </FormRadioGroup.Input>
+      <FormRadioGroup.Input id="inputId1">radio 1</FormRadioGroup.Input>
+      <FormRadioGroup.Input id="inputId2">radio 2</FormRadioGroup.Input>
+      <FormRadioGroup.Input id="inputId3">radio 3</FormRadioGroup.Input>
+      <FormRadioGroup.ValidationMessage isValid id="xxxx">
+        Validé
+      </FormRadioGroup.ValidationMessage>
     </FormRadioGroup>
   );
 };
 
-export const Valid: ComponentStory<typeof FormRadioGroup> = args => {
+export const IsError: ComponentStory<typeof FormRadioGroup> = args => {
   return (
-    <FormRadioGroup isValid {...args}>
+    <FormRadioGroup isError ariaLabelledby="legendId oooo" {...args}>
       <FormRadioGroup.Legend id="legendId">Légende</FormRadioGroup.Legend>
-      <FormRadioGroup.Input id="inputId1" size="sm">
-        radio 1
-      </FormRadioGroup.Input>
-      <FormRadioGroup.Input id="inputId2" size="sm">
-        radio 2
-      </FormRadioGroup.Input>
-      <FormRadioGroup.Input id="inputId3" size="sm">
-        radio 3
-      </FormRadioGroup.Input>
-      <FormRadioGroup.ValidationMessage isValid>Validé</FormRadioGroup.ValidationMessage>
-    </FormRadioGroup>
-  );
-};
-
-export const Error: ComponentStory<typeof FormRadioGroup> = args => {
-  return (
-    <FormRadioGroup isError {...args}>
-      <FormRadioGroup.Legend id="legendId">Légende</FormRadioGroup.Legend>
-      <FormRadioGroup.Input id="inputId1" size="sm">
-        radio 1
-      </FormRadioGroup.Input>
-      <FormRadioGroup.Input id="inputId2" size="sm">
-        radio 2
-      </FormRadioGroup.Input>
-      <FormRadioGroup.Input id="inputId3" size="sm">
-        radio 3
-      </FormRadioGroup.Input>
-      <FormRadioGroup.ValidationMessage isError>Error</FormRadioGroup.ValidationMessage>
+      <FormRadioGroup.Input id="inputId1">radio 1</FormRadioGroup.Input>
+      <FormRadioGroup.Input id="inputId2">radio 2</FormRadioGroup.Input>
+      <FormRadioGroup.Input id="inputId3">radio 3</FormRadioGroup.Input>
+      <FormRadioGroup.ValidationMessage isError id="oooo">
+        Error
+      </FormRadioGroup.ValidationMessage>
     </FormRadioGroup>
   );
 };
--- packages/app/src/design-system/base/FormRadio.tsx
+++ packages/app/src/design-system/base/FormRadio.tsx
@@ -4,38 +4,55 @@ import React, { Children } from "react";
 
 import type { AuthorizedChildType } from "../utils/compatible-components";
 import { compatibleComponents } from "../utils/compatible-components";
+import { Box } from "./Box";
+import type { MarginProps } from "design-system/utils/spacing";
 
 export type FormRadioGroupProps = PropsWithChildren<
-  React.ReactHTMLElement<HTMLDivElement> & {
+  Omit<MarginProps, "ml" | "mr" | "mx"> & {
+    ariaLabelledby?: string;
+    inline?: boolean;
     isDisabled?: boolean;
     isError?: boolean;
     isValid?: boolean;
+    size?: "md" | "sm";
   }
 >;
 
-export const FormRadioGroup = ({ children, isError, isValid, isDisabled }: FormRadioGroupProps) => {
+export const FormRadioGroup = ({
+  children,
+  isError,
+  isValid,
+  isDisabled,
+  size = "md",
+  inline,
+  ariaLabelledby,
+  ...rest
+}: FormRadioGroupProps) => {
   const arrayOfChildren = Children.toArray(children);
   compatibleComponents(["FormRadioInput", "FormRadioLegend", "ValidationMessage"], arrayOfChildren);
   const legend = arrayOfChildren.filter(child => (child as AuthorizedChildType).type.name === "FormRadioLegend");
   const content = arrayOfChildren.filter(child => (child as AuthorizedChildType).type.name === "FormRadioInput");
   const validation = arrayOfChildren.filter(child => (child as AuthorizedChildType).type.name === "ValidationMessage");
-
   return (
-    <div className="fr-form-group">
+    <Box
+      className={clsx("fr-form-group", size === "sm" && "fr-radio-group--sm", inline && "fr-fieldset--inline")}
+      {...rest}
+    >
       <fieldset
         className={clsx("fr-fieldset", isError && "fr-fieldset--error", isValid && "fr-fieldset--valid")}
         disabled={isDisabled}
         role="group"
+        aria-labelledby={ariaLabelledby}
       >
         {legend}
         <div className="fr-fieldset__content">{content}</div>
         {validation.length > 0 && validation}
       </fieldset>
-    </div>
+    </Box>
   );
 };
 
-FormRadioGroup.Legend = function FormRadioLegend({ children, id }: { children: ReactNode; id: string }) {
+FormRadioGroup.Legend = function FormRadioLegend({ children, id }: PropsWithChildren<{ id: string }>) {
   return (
     <legend className="fr-fieldset__legend fr-text--regular" id={id}>
       {children}
@@ -43,18 +60,9 @@ FormRadioGroup.Legend = function FormRadioLegend({ children, id }: { children: R
   );
 };
 
-FormRadioGroup.Input = function FormRadioInput({
-  children,
-  id,
-  size,
-}: {
-  children: ReactNode;
-  id: string;
-  size?: "md" | "sm";
-}) {
-  const sizeClass = size ? size : "md";
+FormRadioGroup.Input = function FormRadioInput({ children, id }: { children: ReactNode; id: string }) {
   return (
-    <div className={clsx("fr-radio-group", `fr-radio-group--${sizeClass}`)}>
+    <div className="fr-radio-group">
       <input id={id} type="radio" />
       <label className="fr-label" htmlFor={id}>
         {children}
@@ -67,16 +75,14 @@ FormRadioGroup.ValidationMessage = function ValidationMessage({
   children,
   isError,
   isValid,
-}: {
-  children: ReactNode;
+  id,
+}: PropsWithChildren<{
+  id: string;
   isError?: boolean;
   isValid?: boolean;
-}) {
+}>) {
   return (
-    <p
-      id="radio-valid-desc-valid"
-      className={clsx("fr-fieldset", isError && "fr-error-text", isValid && "fr-valid-text")}
-    >
+    <p id={id} className={clsx(isError && "fr-error-text", isValid && "fr-valid-text")}>
       {children}
     </p>
   );
