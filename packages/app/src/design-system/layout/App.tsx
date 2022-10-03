001865edeb782b93365a0414a075caab905aa3b0
Laurent Sutterlity <laurent@sutterlity.studio>
2022-10-03 11:29:56 +0200

    refactor: missing replace FunctionComponent by PropsWithChildren

--- packages/app/src/design-system/layout/App.tsx
+++ packages/app/src/design-system/layout/App.tsx
@@ -1,10 +1,10 @@
-import type { FunctionComponent } from "react";
+import type { PropsWithChildren } from "react";
 
 import { Footer } from "../base/Footer";
 import { Header } from "../base/Header";
 import styles from "./App.module.css";
 
-export const App: FunctionComponent = ({ children }) => {
+export const App = ({ children }: PropsWithChildren<Record<string, unknown>>) => {
   return (
     <div className={styles.app}>
       <Header />
