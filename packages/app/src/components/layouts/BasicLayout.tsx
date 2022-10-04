09886b08a80a1e962c66d7ecf4991d98c2c750c3
Sutterlity Laurent <laurent@sutterlity.studio>
2022-10-04 11:56:38 +0200

    feat: add chapo pages (#1098 ) (#1107)

---
+++ packages/app/src/components/layouts/BasicLayout.tsx
@@ -0,0 +1,15 @@
+import "@gouvfr/dsfr/dist/dsfr.main.min.css";
+import "@gouvfr/dsfr/dist/utility/icons/icons-system/icons-system.min.css";
+import "@gouvfr/dsfr/dist/utility/icons/icons-user/icons-user.min.css";
+import "@gouvfr/dsfr/dist/utility/icons/icons-business/icons-business.min.css";
+
+import type { PropsWithChildren } from "react";
+
+import { App } from "@design-system";
+
+// Layout for unauthenticated users.
+
+// eslint-disable-next-line @typescript-eslint/ban-types -- no props
+export const BasicLayout = ({ children }: PropsWithChildren<{}>) => {
+  return <App>{children}</App>;
+};
