a63a1947dbb764c4f292381c9a09a8e21997ebe7
Sutterlity Laurent <laurent@sutterlity.studio>
2022-10-07 15:30:29 +0200

    fix: add new logo (#1126)

--- packages/simulateur/src/components/ds/Logo.tsx
+++ packages/simulateur/src/components/ds/Logo.tsx
@@ -7,7 +7,7 @@ const Logo: FunctionComponent<LogoProps> = () => {
   return (
     <Image
       src={process.env.PUBLIC_URL + "/marianne.svg"}
-      alt="Aller à la page d'accueil du Ministère du travail, de l'emploi et de l'insertion"
+      alt="Aller à la page d'accueil du Ministère du travail, du plein emploi et de l'insertion"
       width={88}
     />
   )
