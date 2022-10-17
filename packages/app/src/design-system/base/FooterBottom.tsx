37311a9f03ab8c2ee8a1485efce4321aeea59f89
Sutterlity Laurent <laurent@sutterlity.studio>
2022-10-17 16:09:10 +0200

    feat: add footer links (#1184)

--- packages/app/src/design-system/base/FooterBottom.tsx
+++ packages/app/src/design-system/base/FooterBottom.tsx
@@ -7,8 +7,8 @@ export const FooterBottom = ({ children }: PropsWithChildren) => {
       <div className="fr-footer__bottom-copy">
         <p>
           Sauf mention contraire, tous les contenus de ce site sont sous{" "}
-          <a href="https://github.com/etalab/licence-ouverte/blob/master/LO.md" target="_blank" rel="noreferrer">
-            licence etalab-2.0
+          <a href="https://github.com/SocialGouv/egapro/blob/master/LICENSE" target="_blank" rel="noreferrer">
+            licence Apache 2.0
           </a>
         </p>
       </div>
