399fc44cb812b6743bcd243e1021f436ff991664
Pierre-Olivier Mauguet <pierre-olivier.mauguet@beta.gouv.fr>
2022-10-18 11:32:37 +0200

    feat: when user disconects, formData is removed (#1188)
    * feat: when user disconects, formData is removed
    
    * fix: warning react for uncontrolled to controlled component

--- packages/app/src/pages/ecart-rep/entreprise.tsx
+++ packages/app/src/pages/ecart-rep/entreprise.tsx
@@ -11,12 +11,11 @@ const title = "Informations entreprise";
 
 const InformationsEntreprise: NextPageWithLayout = () => {
   useUser({ redirectTo: "/ecart-rep/email" });
+  const router = useRouter();
 
   // No need to use React Hook Form here, because we only show read only data.
   const { formData } = useFormManager();
   const { config } = useConfig();
-
-  const router = useRouter();
 
   const REGIONS = config?.REGIONS;
   const DEPARTEMENTS = config?.DEPARTEMENTS;
@@ -41,39 +40,39 @@ const InformationsEntreprise: NextPageWithLayout = () => {
         <FormLayout>
           <FormGroup>
             <FormGroupLabel htmlFor="siren">Siren</FormGroupLabel>
-            <FormInput id="siren" type="text" readOnly value={formData.entreprise?.siren} />
+            <FormInput id="siren" type="text" readOnly value={formData.entreprise?.siren || ""} />
           </FormGroup>
           <FormGroup>
             <FormGroupLabel htmlFor="raison-sociale">Raison sociale de l'entreprise</FormGroupLabel>
-            <FormInput id="raison-sociale" type="text" readOnly value={formData.entreprise?.raison_sociale} />
+            <FormInput id="raison-sociale" type="text" readOnly value={formData.entreprise?.raison_sociale || ""} />
           </FormGroup>
           <FormGroup>
             <FormGroupLabel htmlFor="code-naf">Code NAF</FormGroupLabel>
-            <FormInput id="code-naf" type="text" readOnly value={nafLabel} />
+            <FormInput id="code-naf" type="text" readOnly value={nafLabel || ""} />
           </FormGroup>
           <FormGroup>
             <FormGroupLabel htmlFor="region">Région</FormGroupLabel>
-            <FormInput id="region" type="text" readOnly value={regionLabel} />
+            <FormInput id="region" type="text" readOnly value={regionLabel || ""} />
           </FormGroup>
           <FormGroup>
             <FormGroupLabel htmlFor="departement">Département</FormGroupLabel>
-            <FormInput id="departement" type="text" readOnly value={departementLabel} />
+            <FormInput id="departement" type="text" readOnly value={departementLabel || ""} />
           </FormGroup>
           <FormGroup>
             <FormGroupLabel htmlFor="adresse">Adresse</FormGroupLabel>
-            <FormInput id="adresse" type="text" readOnly value={formData.entreprise?.adresse} />
+            <FormInput id="adresse" type="text" readOnly value={formData.entreprise?.adresse || ""} />
           </FormGroup>
           <FormGroup>
             <FormGroupLabel htmlFor="code-postal">Code postal</FormGroupLabel>
-            <FormInput id="code-postal" type="text" readOnly value={formData.entreprise?.code_postal} />
+            <FormInput id="code-postal" type="text" readOnly value={formData.entreprise?.code_postal || ""} />
           </FormGroup>
           <FormGroup>
             <FormGroupLabel htmlFor="commune">Commune</FormGroupLabel>
-            <FormInput id="commune" type="text" readOnly value={formData.entreprise?.commune} />
+            <FormInput id="commune" type="text" readOnly value={formData.entreprise?.commune || ""} />
           </FormGroup>
           <FormGroup>
             <FormGroupLabel htmlFor="code-pays">Code pays</FormGroupLabel>
-            <FormInput id="code-pays" type="text" readOnly value={formData.entreprise?.code_pays} />
+            <FormInput id="code-pays" type="text" readOnly value={formData.entreprise?.code_pays || ""} />
           </FormGroup>
           <FormLayoutButtonGroup>
             <FormButton type="button" variant="secondary" onClick={() => router.push("/ecart-rep/declarant")}>
