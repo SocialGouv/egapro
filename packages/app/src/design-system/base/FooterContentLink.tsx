79ced450dc69016838f590ce888d25b578e02343
Laurent Sutterlity <laurent@sutterlity.studio>
2022-10-20 15:27:04 +0200

    refactor: refactoring footer component

--- packages/app/src/design-system/base/FooterContentLink.tsx
+++
@@ -1,9 +0,0 @@
-export type FooterContentLinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement>;
-
-export const FooterContentLink = ({ children, ...rest }: FooterContentLinkProps) => {
-  return (
-    <a className="fr-footer__content-link" {...rest}>
-      {children}
-    </a>
-  );
-};
