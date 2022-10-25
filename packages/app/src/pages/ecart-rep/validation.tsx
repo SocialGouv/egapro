603aeeb6ed886e6dd534ddc962484726094d0a3c
pom421 <mauguet.po@gmail.com>
2022-10-25 14:45:51 +0200

    feat: handling error in validation page

--- packages/app/src/pages/ecart-rep/validation.tsx
+++ packages/app/src/pages/ecart-rep/validation.tsx
@@ -1,14 +1,16 @@
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
-import type { FormEvent } from "react";
+import { useState } from "react";
 import type { NextPageWithLayout } from "../_app";
 import {
   motifNonCalculabiliteCadresOptions,
   motifNonCalculabiliteMembresOptions,
 } from "@common/models/repartition-equilibree";
-import { ClientAuthenticatedOnly } from "@components/ClientAuthenticatedOnly";
+import { ClientOnly } from "@components/ClientOnly";
 import { RepartitionEquilibreeLayout } from "@components/layouts/RepartitionEquilibreeLayout";
 import {
+  Alert,
   ButtonAsLink,
   FormButton,
   FormLayout,
@@ -20,13 +22,17 @@ import {
   RecapSectionItems,
   RecapSectionTitle,
 } from "@design-system";
-import { useFormManager, putRepartition } from "@services/apiClient";
+import { useFormManager, putRepartitionEquilibree } from "@services/apiClient";
 
 const title = "Validation de vos écarts";
+
+const SERVER_ERROR = "Problème lors de l'envoi de la répartition équilibrée.";
 
 const Validation: NextPageWithLayout = () => {
   const router = useRouter();
   const { formData } = useFormManager();
+  const [globalError, setGlobalError] = useState("");
+  const [animationParent] = useAutoAnimate<HTMLDivElement>();
 
   const formatMotif = (motif: string): string | undefined => {
     const found =
@@ -36,10 +42,15 @@ const Validation: NextPageWithLayout = () => {
     return found?.label;
   };
 
-  const validateForm = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    putRepartition(formData);
-    router.push("/ecart-rep/transmission");
+  const sendRepartitionEquilibree = async () => {
+    try {
+      await putRepartitionEquilibree(formData);
+      router.push("/ecart-rep/transmission");
+    } catch (error) {
+      console.error(error);
+      setGlobalError(SERVER_ERROR);
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
   };
 
   const previousPage =
@@ -48,8 +59,13 @@ const Validation: NextPageWithLayout = () => {
       : "/ecart-rep/publication";
 
   return (
-    <>
+    <ClientOnly>
       <h1>{title}</h1>
+
+      <div ref={animationParent} style={{ marginBottom: 20 }}>
+        {globalError && <Alert type="error">{globalError}</Alert>}
+      </div>
+
       <p>
         Vous êtes sur le point de valider la procédure vous permettant de transmettre aux services du ministre chargé du
         travail vos écarts éventuels de représentation femmes-hommes conformément aux dispositions de l’
@@ -67,140 +83,134 @@ const Validation: NextPageWithLayout = () => {
         accusé de réception par email.
       </p>
       <h2 className="fr-mt-6w">Récapitulatif</h2>
-      <ClientAuthenticatedOnly>
-        <p>
-          Déclaration des écarts de représentation Femmes/Hommes pour l’année {formData.year && formData.year + 1} au
-          titre des données {formData.year}.
-        </p>
-        <RecapSection>
-          <RecapSectionTitle>Informations déclarant</RecapSectionTitle>
+
+      <p>
+        Déclaration des écarts de représentation Femmes/Hommes pour l’année {formData.year && formData.year + 1} au
+        titre des données {formData.year}.
+      </p>
+      <RecapSection>
+        <RecapSectionTitle>Informations déclarant</RecapSectionTitle>
+        <RecapSectionItems>
+          <RecapSectionItem>
+            <RecapSectionItemLegend>Nom Prénom</RecapSectionItemLegend>
+            <RecapSectionItemContent>
+              {formData.declarant.nom}&nbsp;{formData.declarant.prenom}
+            </RecapSectionItemContent>
+          </RecapSectionItem>
+          <RecapSectionItem>
+            <RecapSectionItemLegend>Adresse email</RecapSectionItemLegend>
+            <RecapSectionItemContent>{formData.declarant.email}</RecapSectionItemContent>
+          </RecapSectionItem>
+        </RecapSectionItems>
+      </RecapSection>
+      <RecapSection>
+        <RecapSectionTitle>Informations entreprise</RecapSectionTitle>
+        <RecapSectionItems>
+          <RecapSectionItem>
+            <RecapSectionItemLegend>Raison sociale</RecapSectionItemLegend>
+            <RecapSectionItemContent>{formData.entreprise?.raison_sociale}</RecapSectionItemContent>
+          </RecapSectionItem>
+          <RecapSectionItem>
+            <RecapSectionItemLegend>Siren</RecapSectionItemLegend>
+            <RecapSectionItemContent>{formData.entreprise?.siren}</RecapSectionItemContent>
+          </RecapSectionItem>
+          <RecapSectionItem>
+            <RecapSectionItemLegend>Code NAF</RecapSectionItemLegend>
+            <RecapSectionItemContent>{formData.entreprise?.code_naf}</RecapSectionItemContent>
+          </RecapSectionItem>
+          <RecapSectionItem>
+            <RecapSectionItemLegend>Adresse</RecapSectionItemLegend>
+            <RecapSectionItemContent>{formData.entreprise?.adresse}</RecapSectionItemContent>
+          </RecapSectionItem>
+        </RecapSectionItems>
+      </RecapSection>
+      <RecapSection>
+        <RecapSectionTitle>Période de référence</RecapSectionTitle>
+        <RecapSectionItems>
+          <RecapSectionItem>
+            <RecapSectionItemLegend>Année au titre de laquelle les écarts sont calculés</RecapSectionItemLegend>
+            <RecapSectionItemContent>{formData.year}</RecapSectionItemContent>
+          </RecapSectionItem>
+          <RecapSectionItem>
+            <RecapSectionItemLegend>
+              Date de fin de la période de douze mois consécutifs correspondant à l’exercice comptable pour le calcul
+              des écarts
+            </RecapSectionItemLegend>
+            <RecapSectionItemContent>{formData.endOfPeriod}</RecapSectionItemContent>
+          </RecapSectionItem>
+        </RecapSectionItems>
+      </RecapSection>
+      <RecapSection>
+        <RecapSectionTitle>Écart de représentation parmi les cadres dirigeants</RecapSectionTitle>
+        {formData.motifEcartsCadresNonCalculable ? (
           <RecapSectionItems>
             <RecapSectionItem>
-              <RecapSectionItemLegend>Nom Prénom</RecapSectionItemLegend>
-              <RecapSectionItemContent>
-                {formData.declarant.nom}&nbsp;{formData.declarant.prenom}
-              </RecapSectionItemContent>
-            </RecapSectionItem>
-            <RecapSectionItem>
-              <RecapSectionItemLegend>Adresse email</RecapSectionItemLegend>
-              <RecapSectionItemContent>{formData.declarant.email}</RecapSectionItemContent>
+              <RecapSectionItemLegend>Motif de non calculabilité</RecapSectionItemLegend>
+              <RecapSectionItemContent>{formatMotif(formData.motifEcartsCadresNonCalculable)}</RecapSectionItemContent>
             </RecapSectionItem>
           </RecapSectionItems>
-        </RecapSection>
-        <RecapSection>
-          <RecapSectionTitle>Informations entreprise</RecapSectionTitle>
+        ) : (
           <RecapSectionItems>
             <RecapSectionItem>
-              <RecapSectionItemLegend>Raison sociale</RecapSectionItemLegend>
-              <RecapSectionItemContent>{formData.entreprise?.raison_sociale}</RecapSectionItemContent>
+              <RecapSectionItemLegend>Pourcentage de femmes parmi les cadres dirigeants</RecapSectionItemLegend>
+              <RecapSectionItemContent>{formData.ecartsCadresFemmes}&nbsp;%</RecapSectionItemContent>
             </RecapSectionItem>
             <RecapSectionItem>
-              <RecapSectionItemLegend>Siren</RecapSectionItemLegend>
-              <RecapSectionItemContent>{formData.entreprise?.siren}</RecapSectionItemContent>
-            </RecapSectionItem>
-            <RecapSectionItem>
-              <RecapSectionItemLegend>Code NAF</RecapSectionItemLegend>
-              <RecapSectionItemContent>{formData.entreprise?.code_naf}</RecapSectionItemContent>
-            </RecapSectionItem>
-            <RecapSectionItem>
-              <RecapSectionItemLegend>Adresse</RecapSectionItemLegend>
-              <RecapSectionItemContent>{formData.entreprise?.adresse}</RecapSectionItemContent>
+              <RecapSectionItemLegend>Pourcentage d’hommes parmi les cadres dirigeants</RecapSectionItemLegend>
+              <RecapSectionItemContent>{formData.ecartsCadresHommes}&nbsp;%</RecapSectionItemContent>
             </RecapSectionItem>
           </RecapSectionItems>
-        </RecapSection>
-        <RecapSection>
-          <RecapSectionTitle>Période de référence</RecapSectionTitle>
+        )}
+      </RecapSection>
+      <RecapSection>
+        <RecapSectionTitle>Écart de représentation parmi les membres des instances dirigeantes</RecapSectionTitle>
+        {formData.motifEcartsMembresNonCalculable ? (
           <RecapSectionItems>
             <RecapSectionItem>
-              <RecapSectionItemLegend>Année au titre de laquelle les écarts sont calculés</RecapSectionItemLegend>
-              <RecapSectionItemContent>{formData.year}</RecapSectionItemContent>
-            </RecapSectionItem>
-            <RecapSectionItem>
-              <RecapSectionItemLegend>
-                Date de fin de la période de douze mois consécutifs correspondant à l’exercice comptable pour le calcul
-                des écarts
-              </RecapSectionItemLegend>
-              <RecapSectionItemContent>{formData.endOfPeriod}</RecapSectionItemContent>
+              <RecapSectionItemLegend>Motif de non calculabilité</RecapSectionItemLegend>
+              <RecapSectionItemContent>{formatMotif(formData.motifEcartsMembresNonCalculable)}</RecapSectionItemContent>
             </RecapSectionItem>
           </RecapSectionItems>
-        </RecapSection>
-        <RecapSection>
-          <RecapSectionTitle>Écart de représentation parmi les cadres dirigeants</RecapSectionTitle>
-          {formData.motifEcartsCadresNonCalculable ? (
-            <RecapSectionItems>
-              <RecapSectionItem>
-                <RecapSectionItemLegend>Motif de non calculabilité</RecapSectionItemLegend>
-                <RecapSectionItemContent>
-                  {formatMotif(formData.motifEcartsCadresNonCalculable)}
-                </RecapSectionItemContent>
-              </RecapSectionItem>
-            </RecapSectionItems>
-          ) : (
-            <RecapSectionItems>
-              <RecapSectionItem>
-                <RecapSectionItemLegend>Pourcentage de femmes parmi les cadres dirigeants</RecapSectionItemLegend>
-                <RecapSectionItemContent>{formData.ecartsCadresFemmes}&nbsp;%</RecapSectionItemContent>
-              </RecapSectionItem>
-              <RecapSectionItem>
-                <RecapSectionItemLegend>Pourcentage d’hommes parmi les cadres dirigeants</RecapSectionItemLegend>
-                <RecapSectionItemContent>{formData.ecartsCadresHommes}&nbsp;%</RecapSectionItemContent>
-              </RecapSectionItem>
-            </RecapSectionItems>
-          )}
-        </RecapSection>
-        <RecapSection>
-          <RecapSectionTitle>Écart de représentation parmi les membres des instances dirigeantes</RecapSectionTitle>
-          {formData.motifEcartsMembresNonCalculable ? (
-            <RecapSectionItems>
-              <RecapSectionItem>
-                <RecapSectionItemLegend>Motif de non calculabilité</RecapSectionItemLegend>
-                <RecapSectionItemContent>
-                  {formatMotif(formData.motifEcartsMembresNonCalculable)}
-                </RecapSectionItemContent>
-              </RecapSectionItem>
-            </RecapSectionItems>
-          ) : (
-            <RecapSectionItems>
-              <RecapSectionItem>
-                <RecapSectionItemLegend>Pourcentage de femmes parmi les cadres dirigeants</RecapSectionItemLegend>
-                <RecapSectionItemContent>{formData.ecartsMembresFemmes}&nbsp;%</RecapSectionItemContent>
-              </RecapSectionItem>
-              <RecapSectionItem>
-                <RecapSectionItemLegend>Pourcentage d’hommes parmi les cadres dirigeants</RecapSectionItemLegend>
-                <RecapSectionItemContent>{formData.ecartsMembresHommes}&nbsp;%</RecapSectionItemContent>
-              </RecapSectionItem>
-            </RecapSectionItems>
-          )}
-        </RecapSection>
-        <RecapSection>
-          <RecapSectionTitle>Publication</RecapSectionTitle>
+        ) : (
           <RecapSectionItems>
             <RecapSectionItem>
-              <RecapSectionItemLegend>Date de publication</RecapSectionItemLegend>
-              <RecapSectionItemContent>{formData.publishingDate}</RecapSectionItemContent>
+              <RecapSectionItemLegend>Pourcentage de femmes parmi les cadres dirigeants</RecapSectionItemLegend>
+              <RecapSectionItemContent>{formData.ecartsMembresFemmes}&nbsp;%</RecapSectionItemContent>
             </RecapSectionItem>
             <RecapSectionItem>
-              <RecapSectionItemLegend>Site internet de publication</RecapSectionItemLegend>
-              <RecapSectionItemContent>{formData.publishingWebsiteUrl}</RecapSectionItemContent>
-            </RecapSectionItem>
-            <RecapSectionItem>
-              <RecapSectionItemLegend>Modalités de communication auprès des salariés</RecapSectionItemLegend>
-              <RecapSectionItemContent>{formData.publishingContent}</RecapSectionItemContent>
+              <RecapSectionItemLegend>Pourcentage d’hommes parmi les cadres dirigeants</RecapSectionItemLegend>
+              <RecapSectionItemContent>{formData.ecartsMembresHommes}&nbsp;%</RecapSectionItemContent>
             </RecapSectionItem>
           </RecapSectionItems>
-        </RecapSection>
-        <form noValidate onSubmit={e => validateForm(e)}>
-          <FormLayout>
-            <FormLayoutButtonGroup>
-              <NextLink href={previousPage} passHref>
-                <ButtonAsLink variant="secondary">Précédent</ButtonAsLink>
-              </NextLink>
-              <FormButton type="submit">Valider et transmettre les résultats</FormButton>
-            </FormLayoutButtonGroup>
-          </FormLayout>
-        </form>
-      </ClientAuthenticatedOnly>
-    </>
+        )}
+      </RecapSection>
+      <RecapSection>
+        <RecapSectionTitle>Publication</RecapSectionTitle>
+        <RecapSectionItems>
+          <RecapSectionItem>
+            <RecapSectionItemLegend>Date de publication</RecapSectionItemLegend>
+            <RecapSectionItemContent>{formData.publishingDate}</RecapSectionItemContent>
+          </RecapSectionItem>
+          <RecapSectionItem>
+            <RecapSectionItemLegend>Site internet de publication</RecapSectionItemLegend>
+            <RecapSectionItemContent>{formData.publishingWebsiteUrl}</RecapSectionItemContent>
+          </RecapSectionItem>
+          <RecapSectionItem>
+            <RecapSectionItemLegend>Modalités de communication auprès des salariés</RecapSectionItemLegend>
+            <RecapSectionItemContent>{formData.publishingContent}</RecapSectionItemContent>
+          </RecapSectionItem>
+        </RecapSectionItems>
+      </RecapSection>
+
+      <FormLayout>
+        <FormLayoutButtonGroup>
+          <NextLink href={previousPage} passHref>
+            <ButtonAsLink variant="secondary">Précédent</ButtonAsLink>
+          </NextLink>
+          <FormButton onClick={sendRepartitionEquilibree}>Valider et transmettre les résultats</FormButton>
+        </FormLayoutButtonGroup>
+      </FormLayout>
+    </ClientOnly>
   );
 };
 
