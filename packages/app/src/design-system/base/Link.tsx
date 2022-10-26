45eb1fb82a0ec9692434b3fdbc167ebc76acd251
Laurent Sutterlity <laurent@sutterlity.studio>
2022-10-26 14:52:30 +0200

    fix: add missing forward ref

--- packages/app/src/design-system/base/Link.tsx
+++ packages/app/src/design-system/base/Link.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import { forwardRef } from "react";
 
 import type { IconStyles } from "../utils/icon-styles";
 
@@ -10,34 +11,31 @@ export type LinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
   size?: "lg" | "sm";
 };
 
-export const Link = ({
-  href,
-  children,
-  size,
-  iconLeft,
-  iconRight,
-  target,
-  isDisabled,
-  isCurrent,
-  ...rest
-}: LinkProps) => {
-  return (
-    <a
-      href={href || undefined}
-      aria-current={isCurrent ? "page" : undefined}
-      aria-disabled={isDisabled || !href ? true : undefined}
-      className={clsx(
-        "fr-link",
-        size === "sm" && "fr-link--sm",
-        size === "lg" && "fr-link--lg",
-        iconLeft && `fr-link--icon-left ${iconLeft}`,
-        iconRight && `fr-link--icon-right ${iconRight}`,
-      )}
-      target={target}
-      rel={target === "_blank" ? "noopener noreferrer" : undefined}
-      {...rest}
-    >
-      {children}
-    </a>
-  );
-};
+export type LinkRef = HTMLAnchorElement;
+
+export const Link = forwardRef<LinkRef, LinkProps>(
+  ({ href, children, size, iconLeft, iconRight, target, isDisabled, isCurrent, ...rest }, ref) => {
+    return (
+      <a
+        ref={ref}
+        href={href || undefined}
+        aria-current={isCurrent ? "page" : undefined}
+        aria-disabled={isDisabled || !href ? true : undefined}
+        className={clsx(
+          "fr-link",
+          size === "sm" && "fr-link--sm",
+          size === "lg" && "fr-link--lg",
+          iconLeft && `fr-link--icon-left ${iconLeft}`,
+          iconRight && `fr-link--icon-right ${iconRight}`,
+        )}
+        target={target}
+        rel={target === "_blank" ? "noopener noreferrer" : undefined}
+        {...rest}
+      >
+        {children}
+      </a>
+    );
+  },
+);
+
+Link.displayName = "Link";
