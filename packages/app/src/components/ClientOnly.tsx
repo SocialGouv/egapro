92ddeff41a113e538d56a5f26792ab0d4934fd47
pom421 <mauguet.po@gmail.com>
2022-10-27 17:58:52 +0200

    fix: problem hydratation react (#1262)

--- packages/app/src/components/ClientOnly.tsx
+++ packages/app/src/components/ClientOnly.tsx
@@ -1,11 +1,13 @@
 import type { PropsWithChildren } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
-import { useMountedState } from "react-use";
 
 export const ClientOnly = ({ children, ...delegated }: PropsWithChildren<unknown>) => {
-  const isMounted = useMountedState();
-
-  if (!isMounted) {
+  const [hasMounted, setHasMounted] = useState(false);
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+  if (!hasMounted) {
     return null;
   }
   return <div {...delegated}>{children}</div>;
