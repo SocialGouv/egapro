166c4a2a0b3bc3b18dab37e2d770da4e115f2072
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2022-11-04 13:11:59 +0100

    chore: pull from master

--- packages/app/__tests__/components/ds/Header.test.tsx
+++ packages/app/__tests__/components/ds/Header.test.tsx
@@ -1,9 +1,5 @@
 import { render } from "@testing-library/react";
-<<<<<<< HEAD
-import { Header } from "@/components/ds/Header";
-=======
 import { Header } from "@components/ds/Header";
->>>>>>> 614a370fd54e75789e0e390e92296f49e9a4eafb
 
 test("should match snapshot", () => {
   const view = render(<Header />);
--- packages/app/__tests__/components/ds/Logo.test.tsx
+++ packages/app/__tests__/components/ds/Logo.test.tsx
@@ -1,9 +1,5 @@
 import { render } from "@testing-library/react";
-<<<<<<< HEAD
-import { Logo } from "@/components/ds/Logo";
-=======
 import { Logo } from "@components/ds/Logo";
->>>>>>> 614a370fd54e75789e0e390e92296f49e9a4eafb
 
 test("should match snapshot", () => {
   const view = render(<Logo />);
