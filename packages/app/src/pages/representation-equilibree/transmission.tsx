910065837a30f87a4ebf8e9ae14bac349d6b82f8
pom421 <mauguet.po@gmail.com>
2022-11-10 15:46:32 +0100

    fix: bug in SSR with transmission page

--- packages/app/src/pages/representation-equilibree/transmission.tsx
+++ packages/app/src/pages/representation-equilibree/transmission.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 
 import type { NextPageWithLayout } from "../_app";
+import { ClientOnly } from "@components/ClientOnly";
 import { RepresentationEquilibreeLayout } from "@components/layouts/RepresentationEquilibreeLayout";
 import type { FormButtonProps } from "@design-system";
 import { TileSuccessTitle } from "@design-system";
@@ -47,64 +48,73 @@ const Transmission: NextPageWithLayout = () => {
   };
 
   return (
-    <TileSuccess>
-      <TileSuccessTitle titleAs="h1">{title}</TileSuccessTitle>
-      <p>
-        Vous avez transmis aux services du ministre chargé du travail vos écarts éventuels de représentation
-        femmes-hommes conformément aux dispositions de l’
-        <a href="https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000045669617" target="_blank" rel="noreferrer">
-          article D. 1142-19 du code du travail
-        </a>
-        .
-      </p>
-      <p>
-        Vous allez recevoir un accusé de réception de votre transmission sur l’email que vous avez déclaré et validé en
-        début de procédure. Cet accusé de réception contient un lien vous permettant de revenir sur votre déclaration.
-      </p>
-      <p>
-        Si vous ne recevez pas cet accusé, merci de bien vérifier que celui-ci n’a pas été déplacé dans votre dossier de
-        courriers indésirables.
-      </p>
-      <p>Nous vous remercions de votre transmission.</p>
-      <Grid mt="6w" justifyCenter>
-        <GridCol md={10} lg={8}>
-          <form>
-            <ButtonGroup>
-              <FormButton type="button" variant="secondary" onClick={sendReceipt} disabled={receiptProcessing}>
-                {receiptProcessing ? "Accusé en cours d'envoi ..." : "Renvoyer l'accusé de réception"}
-              </FormButton>
-              <NextLink href="/representation-equilibree/assujetti/" passHref>
-                <ButtonAsLink onClick={initNewRepresentation}>Effectuer une nouvelle déclaration</ButtonAsLink>
-              </NextLink>
-            </ButtonGroup>
-          </form>
-        </GridCol>
-      </Grid>
-      {formData.entreprise?.siren && (
+    <ClientOnly>
+      <TileSuccess>
+        <TileSuccessTitle titleAs="h1">{title}</TileSuccessTitle>
+        <p>
+          Vous avez transmis aux services du ministre chargé du travail vos écarts éventuels de représentation
+          femmes-hommes conformément aux dispositions de l’
+          <a
+            href="https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000045669617"
+            target="_blank"
+            rel="noreferrer"
+          >
+            article D. 1142-19 du code du travail
+          </a>
+          .
+        </p>
+        <p>
+          Vous allez recevoir un accusé de réception de votre transmission sur l’email que vous avez déclaré et validé
+          en début de procédure. Cet accusé de réception contient un lien vous permettant de revenir sur votre
+          déclaration.
+        </p>
+        <p>
+          Si vous ne recevez pas cet accusé, merci de bien vérifier que celui-ci n’a pas été déplacé dans votre dossier
+          de courriers indésirables.
+        </p>
+        <p>Nous vous remercions de votre transmission.</p>
         <Grid mt="6w" justifyCenter>
           <GridCol md={10} lg={8}>
-            <Card size="sm" isEnlargeLink>
-              <CardBody>
-                <CardBodyContent>
-                  <CardBodyContentTitle>
-                    <a href={`${API_URL}/representation-equilibree/${formData.entreprise?.siren}/${formData.year}/pdf`}>
-                      Télécharger le récapitulatif
-                    </a>
-                  </CardBodyContentTitle>
-                  <CardBodyContentDescription>
-                    {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */}
-                    Année {formData.year! + 1} au titre des données {formData.year}.
-                  </CardBodyContentDescription>
-                  <CardBodyContentEnd>
-                    <CardBodyContentDetails>PDF</CardBodyContentDetails>
-                  </CardBodyContentEnd>
-                </CardBodyContent>
-              </CardBody>
-            </Card>
+            <form>
+              <ButtonGroup>
+                <FormButton type="button" variant="secondary" onClick={sendReceipt} disabled={receiptProcessing}>
+                  {receiptProcessing ? "Accusé en cours d'envoi ..." : "Renvoyer l'accusé de réception"}
+                </FormButton>
+                <NextLink href="/representation-equilibree/assujetti/" passHref>
+                  <ButtonAsLink onClick={initNewRepresentation}>Effectuer une nouvelle déclaration</ButtonAsLink>
+                </NextLink>
+              </ButtonGroup>
+            </form>
           </GridCol>
         </Grid>
-      )}
-    </TileSuccess>
+        {formData.entreprise?.siren && (
+          <Grid mt="6w" justifyCenter>
+            <GridCol md={10} lg={8}>
+              <Card size="sm" isEnlargeLink>
+                <CardBody>
+                  <CardBodyContent>
+                    <CardBodyContentTitle>
+                      <a
+                        href={`${API_URL}/representation-equilibree/${formData.entreprise?.siren}/${formData.year}/pdf`}
+                      >
+                        Télécharger le récapitulatif
+                      </a>
+                    </CardBodyContentTitle>
+                    <CardBodyContentDescription>
+                      {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */}
+                      Année {formData.year! + 1} au titre des données {formData.year}.
+                    </CardBodyContentDescription>
+                    <CardBodyContentEnd>
+                      <CardBodyContentDetails>PDF</CardBodyContentDetails>
+                    </CardBodyContentEnd>
+                  </CardBodyContent>
+                </CardBody>
+              </Card>
+            </GridCol>
+          </Grid>
+        )}
+      </TileSuccess>
+    </ClientOnly>
   );
 };
 
