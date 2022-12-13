92d62747079cc5ad37df3be8437cce828009b078
Laurent Sutterlity <laurent@sutterlity.studio>
2022-12-13 11:12:34 +0100

    fix: typo

--- packages/app/src/pages/ajout-declarant.tsx
+++ packages/app/src/pages/ajout-declarant.tsx
@@ -24,18 +24,18 @@ type FormType = z.infer<typeof formSchema>;
 
 const formSchema = z.object({
   user_email: z.string().email({ message: "L'adresse email est invalide." }),
-  email: z
+  emails: z
     .string()
     .regex(
       /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,10})(,\s*([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,10}))*$/,
       { message: "L'adresse email est invalide." },
     ),
-  siren: z.string().regex(/^\d{9}(,\d{9})*$/, { message: "Le Siren est composé de 9 chiffres sans espace." }),
+  sirens: z.string().regex(/^\d{9}(,\d{9})*$/, { message: "Le Siren est composé de 9 chiffres sans espace." }),
 });
 
 const AddDeclarer: NextPageWithLayout = () => {
   const { user } = useUser();
-  const { formData, saveFormData } = useFormManager();
+  const { formData } = useFormManager();
 
   const {
     register,
@@ -49,9 +49,9 @@ const AddDeclarer: NextPageWithLayout = () => {
 
   const resetAsyncForm = useCallback(async () => {
     reset({
-      siren: formData?.entreprise?.siren,
+      sirens: formData?.entreprise?.siren,
       user_email: user?.email,
-      email: formData?.declarant?.email,
+      emails: formData?.declarant?.email,
     });
   }, [formData?.declarant?.email, formData?.entreprise?.siren, reset, user?.email]);
 
@@ -59,12 +59,8 @@ const AddDeclarer: NextPageWithLayout = () => {
     resetAsyncForm();
   }, [resetAsyncForm]);
 
-  const onSubmit = async ({ user_email, email, siren }: FormType) => {
-    saveFormData({
-      declarant: { email: email.trim(), accord_rgpd: false, nom: "", prenom: "", telephone: "" },
-      entreprise: { siren: siren.trim() },
-    });
-    console.log({ user_email: user_email, email: email.replace(/\s/g, "").split(","), siren: siren.split(",") });
+  const onSubmit = async ({ user_email, emails, sirens }: FormType) => {
+    console.log({ user_email: user_email, emails: emails.replace(/\s/g, "").split(","), sirens: sirens.split(",") });
   };
 
   return (
@@ -88,25 +84,29 @@ const AddDeclarer: NextPageWithLayout = () => {
                       <FormGroupMessage id="nom-message-error">{errors.user_email.message}</FormGroupMessage>
                     )}
                   </FormGroup>
-                  <FormGroup isError={Boolean(errors.siren)}>
+                  <FormGroup isError={Boolean(errors.sirens)}>
                     <FormGroupLabel
                       htmlFor="siren"
                       hint="Le numéro Siren se compose de 9 chiffres sans espace, il est possible d’ajouter plusieurs Siren séparées par des virgules sans espace."
                     >
                       Numéro(s) Siren
                     </FormGroupLabel>
-                    <FormTextarea aria-describedby={"siren-msg"} id="siren" {...register("siren")} />
-                    {errors.siren && <FormGroupMessage id="nom-message-error">{errors.siren.message}</FormGroupMessage>}
+                    <FormTextarea aria-describedby={"siren-msg"} id="siren" {...register("sirens")} />
+                    {errors.sirens && (
+                      <FormGroupMessage id="nom-message-error">{errors.sirens.message}</FormGroupMessage>
+                    )}
                   </FormGroup>
-                  <FormGroup isError={Boolean(errors.email)}>
+                  <FormGroup isError={Boolean(errors.emails)}>
                     <FormGroupLabel
                       htmlFor="declarer-email"
                       hint="Il est possible d’ajouter plusieurs emails séparées par des virgules sans espace."
                     >
                       emails
                     </FormGroupLabel>
-                    <FormTextarea aria-describedby={"declarer-email-msg"} id="declarer-email" {...register("email")} />
-                    {errors.email && <FormGroupMessage id="nom-message-error">{errors.email.message}</FormGroupMessage>}
+                    <FormTextarea aria-describedby={"declarer-email-msg"} id="declarer-email" {...register("emails")} />
+                    {errors.emails && (
+                      <FormGroupMessage id="nom-message-error">{errors.emails.message}</FormGroupMessage>
+                    )}
                   </FormGroup>
                   <FormLayoutButtonGroup>
                     <FormButton isDisabled={!isValid}>Envoyer ma demande</FormButton>
