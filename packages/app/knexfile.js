48197617f5f5d44fc4490a7dbc5c85daea238e0a
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2023-01-05 15:49:57 +0100

    fix: fix ci for tests

--- packages/app/knexfile.js
+++
@@ -1,5 +0,0 @@
-require("ts-node").register();
-require("tsconfig-paths").register();
-require("dotenv").config({ path: ".env.development" });
-
-module.exports = require("./src/api/core-domain/infra/db/knex").knexConfig;
