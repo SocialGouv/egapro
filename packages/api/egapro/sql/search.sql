cb4983394df0d5087457a66e9d62f58cb7898147
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2023-03-09 14:55:43 +0100

    fix: order search results by year instead of declared_at (Fixes #1571)

--- packages/api/egapro/sql/search.sql
+++ packages/api/egapro/sql/search.sql
@@ -1,5 +1,5 @@
 SELECT
-    array_agg(declaration.data ORDER BY declaration.declared_at DESC) as data,
+    array_agg(declaration.data ORDER BY declaration.year DESC) as data,
     jsonb_object_agg(declaration.year::text, (declaration.data->'déclaration'->>'index')::int) as notes,
     jsonb_object_agg(declaration.year::text, (declaration.data->'indicateurs'->'rémunérations'->>'note')::int) as notes_remunerations,
     jsonb_object_agg(declaration.year::text, (declaration.data->'indicateurs'->'augmentations'->>'note')::int) as notes_augmentations,
@@ -11,6 +11,6 @@ FROM declaration
 JOIN search ON declaration.siren=search.siren AND declaration.year=search.year
     {where}
 GROUP BY declaration.siren
-ORDER BY max(declaration.declared_at) DESC
+ORDER BY max(declaration.year) DESC
 LIMIT $1
 OFFSET $2
