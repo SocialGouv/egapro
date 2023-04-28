b36692ef46a9baf14f1257557abcb9b6036e8378
pom421 <mauguet.po@gmail.com>
2023-04-28 18:20:03 +0200

    feat: update sql query in raw sql

--- packages/app/scripts/sql-scripts/migrate-simulations.ts
+++ packages/app/scripts/sql-scripts/migrate-simulations.ts
@@ -13,16 +13,28 @@ console.log("table", table);
 export async function migrateSimulations() {
   // On ne s'occupe que des données qui n'ont pas encore la propriété data.indicateurUn.modaliteCalculformValidated, qui sont donc celles avant la migration.
   const rows =
-    (await sql`select * from ${table} where data->'declaration'->>'formValidated' = 'Valid' and not(data->'indicateurUn' ? 'modaliteCalculformValidated') limit 1`) as Simulation[];
+    (await sql`select * from ${table} where data->'indicateurUn'->>'autre' = 'true' and data->'declaration'->>'formValidated' = 'Valid' and not(data->'indicateurUn' ? 'modaliteCalculformValidated')`) as Simulation[];
 
   console.log("nb rows", rows?.length ?? "no rows");
 
-  rows.forEach(row => {
+  rows.forEach(async row => {
+    // console.log("row avant", JSON.stringify(row, null, 2));
+
+    console.log("row.id", row.id);
+
     const newIndicateurUn = updateIndicateurUn(row.data.indicateurUn);
-    // Update sql with new data.
-    sql`update ${table} set data = jsonb_set(data, '{indicateurUn}', ${JSON.stringify(
-      newIndicateurUn,
-    )}::jsonb) where id = ${row.id}`;
+
+    const newIndicateurUnJson = JSON.stringify(newIndicateurUn).replace(/'/g, "''"); // Need to escape single quotes for Postgres.
+
+    // Postgres.js is not able to handle jsonb_set, so we use sql.unsafe instead.
+
+    // const nbUpdates = await sql`update ${table} set data = jsonb_set(data, '{indicateurUn}', ${sql(
+    //   newIndicateurUn,
+    // )}) where id = ${row.id}`;
+
+    await sql.unsafe(
+      `update simulation set data = jsonb_set(data, '{indicateurUn}', '${newIndicateurUnJson}'::jsonb) where id = '${row.id}'`,
+    );
   });
 }
 
