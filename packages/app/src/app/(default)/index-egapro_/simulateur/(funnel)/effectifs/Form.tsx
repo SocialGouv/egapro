85f70956ccd0ea3549944b2f6ce884108ab63595
Laurent Sutterlity <laurent@sutterlity.studio>
2023-06-08 10:42:34 +0200

    feat: replace native dom element by design system component inside simulator launch page

--- packages/app/src/app/(default)/index-egapro_/simulateur/(funnel)/effectifs/Form.tsx
+++ packages/app/src/app/(default)/index-egapro_/simulateur/(funnel)/effectifs/Form.tsx
@@ -1,5 +1,6 @@
+import Alert from "@codegouvfr/react-dsfr/Alert";
 import RadioButtons from "@codegouvfr/react-dsfr/RadioButtons";
-import { FormLayout } from "@design-system";
+import { FormLayout, Link } from "@design-system";
 
 export const EffectifsForm = () => {
   return (
@@ -21,6 +22,32 @@ export const EffectifsForm = () => {
               nativeInputProps: {},
             },
           ]}
+        />
+        <Alert
+          small
+          severity="info"
+          description={
+            <>
+              Pour la tranche d'effectifs assujettis, l’entreprise ou l’unité économique et sociale (UES) doit définir
+              son assujettissement chaque année à la date de l’obligation de publication de l’index, soit le 1er mars.
+              Le calcul des effectifs assujettis de l’entreprise ou de l’unité économique et sociale (UES) est celui
+              prévu aux articles{" "}
+              <Link
+                href="https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000006900783/2008-05-01#:~:text=2%C2%B0%20Les%20salari%C3%A9s%20titulaires,l'entreprise%20%C3%A0%20due%20proportion"
+                target="_blank"
+              >
+                L.1111-2
+              </Link>{" "}
+              et{" "}
+              <Link
+                href="https://www.legifrance.gouv.fr/codes/section_lc/LEGITEXT000006072050/LEGISCTA000006177833/#:~:text=3%C2%B0%20Les%20salari%C3%A9s%20%C3%A0,la%20dur%C3%A9e%20conventionnelle%20du%20travail."
+                target="_blank"
+              >
+                L.1111-3
+              </Link>{" "}
+              du code du travail.
+            </>
+          }
         />
       </FormLayout>
     </form>
--- packages/app/src/app/(default)/index-egapro_/simulateur/commencer/page.tsx
+++ packages/app/src/app/(default)/index-egapro_/simulateur/commencer/page.tsx
@@ -1,5 +1,5 @@
 import Button from "@codegouvfr/react-dsfr/Button";
-import { CenteredContainer } from "@design-system";
+import { CenteredContainer, Heading, Text } from "@design-system";
 
 const title = "Commencer";
 
@@ -13,16 +13,31 @@ export const metadata = {
 const CommencerPage = async () => {
   return (
     <CenteredContainer>
-      <h1>{title}</h1>
-      <p>
-        Vous allez commencer le calcul des indicateurs et de l'index de l'égalité professionnelle pour votre entreprise
-        ou unité économique et sociale (UES).
-      </p>
-      <p>
-        Suite au calcul, vous pourrez poursuivre vers la déclaration afin de transmettre les résultats obtenus aux
-        services du ministre chargé du travail en renseignant les autres informations nécessaires à la déclaration.
-      </p>
-      <Button linkProps={{ href: "/index-egapro_/simulateur/effectifs" }}>Suivant</Button>
+      <Heading as="h1" text={title} />
+      <Text
+        className="fr-text--bold"
+        text={
+          <>
+            Vous allez commencer le calcul des indicateurs et de l'index de l'égalité professionnelle pour votre
+            entreprise ou unité économique et sociale (UES).
+          </>
+        }
+      />
+      <Text
+        text={
+          <>
+            Suite au calcul, vous pourrez poursuivre vers la déclaration afin de transmettre les résultats obtenus aux
+            services du ministre chargé du travail en renseignant les autres informations nécessaires à la déclaration.
+          </>
+        }
+      />
+      <Button
+        linkProps={{ href: "/index-egapro_/simulateur/effectifs" }}
+        iconId="fr-icon-arrow-right-line"
+        iconPosition="right"
+      >
+        Suivant
+      </Button>
     </CenteredContainer>
   );
 };
