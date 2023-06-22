64245ccec8b276fdeb2b95039054c308b350f227
Pierre-Olivier Mauguet <pierre-olivier.mauguet@beta.gouv.fr>
2023-06-22 14:51:10 +0200

    feat: refactor commencer page of declaration

--- packages/app/src/app/index-egapro_/declaration/commencer/CommencerForm.tsx
+++ packages/app/src/app/index-egapro_/declaration/commencer/CommencerForm.tsx
@@ -1,80 +1,60 @@
 "use client";
 
-import { Button } from "@codegouvfr/react-dsfr/Button";
-import { Input } from "@codegouvfr/react-dsfr/Input";
+import ButtonsGroup from "@codegouvfr/react-dsfr/ButtonsGroup";
 import { Select } from "@codegouvfr/react-dsfr/Select";
 import { config } from "@common/config";
 import { PUBLIC_YEARS } from "@common/dict";
-import { zodPositiveIntegerSchema, zodSirenSchema } from "@common/utils/form";
-import { MailtoLinkForNonOwner } from "@components/MailtoLink";
-import { GlobalMessage, type Message } from "@components/next13/GlobalMessage";
+import { zodSirenSchema } from "@common/utils/form";
+import { SkeletonForm } from "@components/utils/skeleton/SkeletonForm";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { memoizedFetchSiren } from "@services/apiClient";
 import { fetchDeclaration } from "@services/apiClient/declaration";
 import { useDeclarationFormManager } from "@services/apiClient/useDeclarationFormManager";
 import { DeclarationFormBuilder, type DeclarationFormState } from "@services/form/declaration/DeclarationFormBuilder";
 import { buildEntreprise } from "@services/form/declaration/entreprise";
+import { sortBy } from "lodash";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-const OWNER_ERROR = "Vous n'avez pas les droits sur ce Siren";
-
 const formSchema = z.object({
-  annéeIndicateurs: zodPositiveIntegerSchema, // No control needed because this is a select with options we provide.
+  annéeIndicateurs: z.number(), // No control needed because this is a select with options we provide.
   siren: zodSirenSchema,
 });
 
 // Infer the TS type according to the zod schema.
 type FormType = z.infer<typeof formSchema>;
 
-const buildConfirmMessage = ({ siren, year }: { siren: string; year: string }) =>
-  `Vous avez commencé une déclaration avec le Siren ${siren} pour l'année ${year}. Voulez-vous commencer une nouvelle déclaration et supprimer les données déjà enregistrées ?`;
+const buildConfirmMessage = ({ siren, annéeIndicateurs }: { annéeIndicateurs: number; siren: string }) =>
+  `Vous avez commencé une déclaration avec le Siren ${siren} pour l'année ${annéeIndicateurs}. Voulez-vous commencer une nouvelle déclaration et supprimer les données déjà enregistrées ?`;
 
 export const CommencerForm = () => {
   const router = useRouter();
   const { formData, saveFormData, resetFormData } = useDeclarationFormManager();
-  const [globalMessage, setGlobalMessage] = useState<Message | undefined>(undefined);
 
   const session = useSession();
 
-  const sirenStorage = formData?.commencer?.entrepriseDéclarante?.siren || "";
-  const annéeIndicateursStorage =
-    formData?.commencer?.annéeIndicateurs === undefined ? "" : String(formData?.commencer?.annéeIndicateurs);
+  // assert(
+  //   session.data && session.data?.user?.companies.length,
+  //   "User should have companies in session. The parent page should check it.",
+  // );
 
   const {
     register,
     handleSubmit,
     setValue,
-    watch,
+    reset: resetForm,
     formState: { errors, isValid },
   } = useForm<FormType>({
     mode: "onTouched",
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      siren: sirenStorage,
-      annéeIndicateurs: annéeIndicateursStorage,
-    },
+    defaultValues: formData.commencer,
   });
 
-  const siren = watch("siren");
-  const annéeIndicateurs = watch("annéeIndicateurs");
+  if (!session.data || !session.data?.user?.companies.length) return <SkeletonForm fields={2} />;
 
-  useEffect(() => {
-    // Reset global error message.
-    setGlobalMessage(undefined);
-  }, [siren, annéeIndicateurs, setGlobalMessage]);
-
-  useEffect(() => {
-    // Special case when the user has no ownership on the Siren.
-    if (errors.siren && errors.siren.message === OWNER_ERROR) {
-      setGlobalMessage({ type: "error", description: <MailtoLinkForNonOwner siren={siren} /> });
-    }
-  }, [errors.siren, setGlobalMessage, siren]);
-
-  if (!session || !session.data) return null;
+  const companies = session.data.user.companies;
 
   /**
    * Check if declaration exists, merge it with state if any and save the state the in local storage.
@@ -113,91 +93,132 @@ export const CommencerForm = () => {
     };
   };
 
-  const getNextPage = (data?: DeclarationFormState) =>
+  const nextPage = (data?: DeclarationFormState) =>
     data?._metadata?.status === "edition"
-      ? `${config.base_declaration_url}/${siren}/${annéeIndicateurs}`
+      ? `${config.base_declaration_url}/${data.commencer?.entrepriseDéclarante?.siren}/${data.commencer?.annéeIndicateurs}`
       : `${config.base_declaration_url}/entreprise`;
 
-  const saveAndExit = async ({ annéeIndicateurs: year, siren }: FormType) => {
-    const newData = await prepareDataWithExistingDeclaration(siren, Number(year), session.data.user.tokenApiV1);
+  const saveAndGoNext = async ({ annéeIndicateurs, siren }: FormType) => {
+    // Synchronize the data with declaration if any.
+    const newData = await prepareDataWithExistingDeclaration(
+      siren,
+      Number(annéeIndicateurs),
+      session.data.user.tokenApiV1,
+    );
 
+    // Save in storage (savePageData is not used because we want to save commencer page and _metadata).
     saveFormData(newData);
 
-    const nextPage = getNextPage(newData);
-
-    console.log("nextPage:", nextPage);
-
-    router.push(getNextPage(newData));
+    // Navigate to next page.
+    router.push(nextPage(newData));
   };
 
-  const onSubmit = async ({ annéeIndicateurs: year, siren }: FormType) => {
+  const onSubmit = async ({ annéeIndicateurs, siren }: FormType) => {
+    const { entrepriseDéclarante, annéeIndicateurs: annéeIndicateursStorage } = formData.commencer ?? {};
+
+    const sirenStorage = entrepriseDéclarante?.siren;
+
     // If no data are present in session storage.
-    if (!sirenStorage) {
-      saveAndExit({ siren, annéeIndicateurs: year });
-      return;
+    if (!sirenStorage || !annéeIndicateursStorage) {
+      return await saveAndGoNext({ siren, annéeIndicateurs });
     }
 
-    if (siren !== sirenStorage || year !== annéeIndicateursStorage) {
-      if (confirm(buildConfirmMessage({ siren: sirenStorage, year: annéeIndicateursStorage }))) {
-        // Start a new declaration of representation.
-        resetFormData();
-        saveAndExit({ siren, annéeIndicateurs: year });
-        return;
-      } else {
-        // Rollback to the old Siren.
-        setValue("siren", sirenStorage);
-        return;
-      }
+    // In data are present in session storage and siren and year are unchanged.
+    if (siren === sirenStorage && annéeIndicateurs === annéeIndicateursStorage) {
+      return router.push(nextPage());
     }
 
-    // In this last case, the siren is already in session storage and unchanged and the user wants to continue.
-    router.push(getNextPage());
+    // In data are present in session storage and siren and year are not the same.
+    if (confirm(buildConfirmMessage({ siren: sirenStorage, annéeIndicateurs: annéeIndicateursStorage }))) {
+      // Start a new declaration of representation.
+      resetFormData();
+      await saveAndGoNext({ siren, annéeIndicateurs });
+    } else {
+      // Rollback to the old Siren.
+      setValue("siren", sirenStorage);
+    }
   };
 
-  const confirmResetFormData = () => {
+  const confirmReset = () => {
     if (confirm("Les données ne sont pas sauvegardées, êtes-vous sûr de vouloir réinitialiser le formulaire ?")) {
       resetFormData();
-      setValue("siren", "");
-      setValue("annéeIndicateurs", "");
+      resetForm();
     }
   };
 
   return (
     <>
-      <GlobalMessage message={globalMessage} />
-
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
         {/* <pre>{JSON.stringify(watch(), null, 2)}</pre> */}
         {/* <pre>{formatZodErrors(errors)}</pre> */}
 
         <Select
           label="Année au titre de laquelle les indicateurs sont calculés"
-          nativeSelectProps={{ ...register("annéeIndicateurs") }}
+          state={errors.annéeIndicateurs && "error"}
+          stateRelatedMessage={errors.annéeIndicateurs?.message}
+          nativeSelectProps={{
+            ...register("annéeIndicateurs", {
+              valueAsNumber: true,
+            }),
+          }}
         >
-          <option value="" disabled hidden>
-            Selectionnez une option
+          <option value="" disabled>
+            Sélectionnez une année
           </option>
-          {PUBLIC_YEARS.sort()
-            .reverse()
-            .map(year => (
-              <option value={year} key={year}>
-                {year}
-              </option>
-            ))}
+          {PUBLIC_YEARS.map(year => (
+            <option value={year} key={`year-select-${year}`}>
+              {year}
+            </option>
+          ))}
         </Select>
-        <Input
+
+        <Select
           label="Numéro Siren de l’entreprise ou de l’entreprise déclarant pour le compte de l’UES (Unité Économique et Sociale)"
-          hintText="Le numéro Siren se compose de 9 chiffres"
-          state={errors.siren?.message ? "error" : "default"}
+          state={errors.siren && "error"}
           stateRelatedMessage={errors.siren?.message}
-          nativeInputProps={{ ...register("siren"), placeholder: "Ex: 504920166, 403461742, 403696735", maxLength: 9 }}
-        />
-        <div style={{ display: "flex", gap: 10 }}>
+          nativeSelectProps={{
+            ...register("siren"),
+          }}
+        >
+          <option value="" disabled>
+            Sélectionnez une entreprise
+          </option>
+          {sortBy(companies, "siren").map(company => (
+            <option key={company.siren} value={company.siren}>
+              {company.siren}
+              {company.label ? ` (${company.label})` : ""}
+            </option>
+          ))}
+        </Select>
+
+        {/* <div style={{ display: "flex", gap: 10 }}>
           <Button type="reset" priority="secondary" disabled={!sirenStorage} onClick={confirmResetFormData}>
             Réinitialiser
           </Button>
           <Button disabled={!isValid}>Suivant</Button>
-        </div>
+        </div> */}
+
+        <ButtonsGroup
+          inlineLayoutWhen="sm and up"
+          buttons={[
+            {
+              children: "Réinitialiser",
+              priority: "secondary",
+              onClick: confirmReset,
+              type: "button",
+              nativeButtonProps: {
+                disabled: formData ? !formData.commencer?.entrepriseDéclarante?.siren : false,
+              },
+            },
+            {
+              children: "Suivant",
+              type: "submit",
+              nativeButtonProps: {
+                disabled: !isValid,
+              },
+            },
+          ]}
+        />
       </form>
     </>
   );
