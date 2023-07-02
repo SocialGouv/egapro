6f5945b66a9c61fbee4a6ad6ed0327a4c32115a8
Sutterlity Laurent <laurent@sutterlity.studio>
2023-07-02 13:21:09 +0200

    feat: add indicatorNote component (#1669)

---
+++ packages/app/src/design-system/base/IndicatorNote.tsx
@@ -0,0 +1,60 @@
+import { fr } from "@codegouvfr/react-dsfr";
+import { cx } from "@codegouvfr/react-dsfr/tools/cx";
+import { type ReactNode } from "react";
+
+import styles from "./IndicatorNote.module.css";
+
+type BaseIndicatorNoteProps = {
+  className?: string;
+  classes?: Partial<Record<"description" | "legend" | "max" | "note" | "root" | "text", string>>;
+  noBorder?: boolean;
+  note: number;
+  text: NonNullable<ReactNode>;
+};
+
+type LargeIndicatorNoteProps = BaseIndicatorNoteProps & {
+  legend: NonNullable<ReactNode>;
+  max: number;
+  size?: "large";
+};
+
+type SmallIndicatorNoteProps = BaseIndicatorNoteProps & {
+  legend?: NonNullable<ReactNode>;
+  max?: number;
+  size?: "small";
+};
+
+export type IndicatorNoteProps = LargeIndicatorNoteProps | SmallIndicatorNoteProps;
+
+export const IndicatorNote = ({
+  note,
+  noBorder,
+  max,
+  text,
+  className,
+  size = "large",
+  legend,
+  classes = {},
+}: IndicatorNoteProps) => (
+  <div
+    className={cx(
+      size === "small" ? styles["tile-small"] : styles.tile,
+      noBorder && styles["tile-no-border"],
+      classes.root,
+      className,
+    )}
+  >
+    <div className={styles["tile-note"]}>
+      <span className={cx(size === "small" ? styles["note-small"] : styles["note"], classes.note)}>{note}</span>
+      {size === "large" && <span className={cx(styles.max, classes.max)}>&nbsp;/&nbsp;{max}</span>}
+    </div>
+    <div className={styles["tile-content"]}>
+      <p className={cx(fr.cx("fr-m-0"), styles.text, classes.text)}>{text}</p>
+      {legend && (
+        <p className={cx(fr.cx("fr-m-0", "fr-text--sm"), classes.legend)}>
+          <i>{legend}</i>
+        </p>
+      )}
+    </div>
+  </div>
+);
