3ea221eef2c664b03755736d012e05655bf212fc
Pierre-Olivier Mauguet <pierre-olivier.mauguet@beta.gouv.fr>
2023-07-10 16:05:29 +0200

    feat: simplify component EditButton

--- packages/app/src/app/(default)/index-egapro_/declaration/declaration-existante/EditButton.tsx
+++ packages/app/src/app/(default)/index-egapro_/declaration/declaration-existante/EditButton.tsx
@@ -4,7 +4,6 @@ import { fr } from "@codegouvfr/react-dsfr";
 import ButtonsGroup from "@codegouvfr/react-dsfr/ButtonsGroup";
 import { type DeclarationDTO } from "@common/models/generated";
 import { useDeclarationFormManager } from "@services/apiClient/useDeclarationFormManager";
-import { DeclarationFormBuilder } from "@services/form/declaration/DeclarationFormBuilder";
 import { useRouter } from "next/navigation";
 
 import { funnelConfig, type FunnelKey } from "../declarationFunnelConfiguration";
@@ -14,11 +13,9 @@ export interface EditButtonProps {
   stepName: FunnelKey;
 }
 
-export const EditButton = ({ declaration, stepName }: EditButtonProps) => {
+export const EditButton = ({ stepName }: EditButtonProps) => {
   const router = useRouter();
-  const { formData, resetFormData, saveFormData, setStatus } = useDeclarationFormManager();
-
-  const declarationForm = DeclarationFormBuilder.buildDeclaration(declaration);
+  const { formData, setStatus } = useDeclarationFormManager();
 
   return (
     <ButtonsGroup
@@ -35,8 +32,6 @@ export const EditButton = ({ declaration, stepName }: EditButtonProps) => {
         {
           children: "Modifier",
           onClick() {
-            resetFormData();
-            saveFormData(declarationForm);
             setStatus("edition");
             router.push(funnelConfig(formData)[stepName].next().url);
           },
