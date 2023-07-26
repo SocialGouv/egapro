4c763d38cfe7b508e779f74c8c2e0d10cb696110
Laurent Sutterlity <laurent@sutterlity.studio>
2023-07-26 10:47:42 +0200

    feat: add BigCote variant NC

--- packages/app/src/design-system/base/BigNote.tsx
+++ packages/app/src/design-system/base/BigNote.tsx
@@ -8,19 +8,25 @@ export type BigNoteProps = {
   className?: string;
   classes?: Partial<Record<"description" | "legend" | "max" | "note" | "root" | "text", string>>;
   legend: NonNullable<ReactNode>;
-  max: number;
+  max?: number;
   noBorder?: boolean;
-  note: number;
-  text: NonNullable<ReactNode>;
+  note?: number;
+  text?: NonNullable<ReactNode>;
 };
 
 export const BigNote = ({ note, noBorder, max, text, className, legend, classes = {} }: BigNoteProps) => (
   <div className={cx(styles.tile, noBorder && styles["tile-no-border"], classes.root, className)}>
     <div className={cx(fr.cx("fr-m-0", "fr-text--sm"), classes.legend)}>{legend}</div>
     <div className={styles["tile-note"]}>
-      <span className={cx(styles["note"], classes.note)}>{note}</span>
-      <span className={cx(styles.max, classes.max)}>&nbsp;/&nbsp;{max}</span>
+      {note !== undefined ? (
+        <>
+          <span className={cx(styles["note"], classes.note)}>{note}</span>
+          {max !== undefined && <span className={cx(styles.max, classes.max)}>&nbsp;/&nbsp;{max}</span>}
+        </>
+      ) : (
+        <span className={cx(styles["note"], classes.note)}>NC</span>
+      )}
     </div>
-    <div className={cx(styles.text, classes.text)}>{text}</div>
+    {text && <div className={cx(styles.text, classes.text)}>{text}</div>}
   </div>
 );
