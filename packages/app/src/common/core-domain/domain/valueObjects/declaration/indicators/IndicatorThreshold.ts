878b1f1358e6bc2f327ae069bbd40fc51513388c
Pierre-Olivier Mauguet <pierre-olivier.mauguet@beta.gouv.fr>
2023-09-20 19:49:13 +0200

    feat: refactor computeIndex

--- packages/app/src/common/core-domain/domain/valueObjects/declaration/indicators/IndicatorThreshold.ts
+++ packages/app/src/common/core-domain/domain/valueObjects/declaration/indicators/IndicatorThreshold.ts
@@ -4,7 +4,17 @@ import { IndicateurDeuxTroisComputer } from "@common/core-domain/computers/Indic
 import { IndicateurQuatreComputer } from "@common/core-domain/computers/IndicateurQuatreComputer";
 import { IndicateurTroisComputer } from "@common/core-domain/computers/IndicateurTroisComputer";
 import { IndicateurUnComputer } from "@common/core-domain/computers/IndicateurUnComputer";
+import { type Company } from "@common/core-domain/domain/declaration/Company";
+import { type HighRemunerationsIndicator } from "@common/core-domain/domain/declaration/indicators/HighRemunerationsIndicator";
+import { type MaternityLeavesIndicator } from "@common/core-domain/domain/declaration/indicators/MaternityLeavesIndicator";
+import { type PromotionsIndicator } from "@common/core-domain/domain/declaration/indicators/PromotionsIndicator";
+import { type RemunerationsIndicator } from "@common/core-domain/domain/declaration/indicators/RemunerationsIndicator";
+import { type SalaryRaisesAndPromotionsIndicator } from "@common/core-domain/domain/declaration/indicators/SalaryRaisesAndPromotionsIndicator";
+import { type SalaryRaisesIndicator } from "@common/core-domain/domain/declaration/indicators/SalaryRaisesIndicator";
 import { type DeclarationDTO, type IndicatorKey } from "@common/core-domain/dtos/DeclarationDTO";
+import assert from "assert";
+
+import { CompanyWorkforceRange } from "../CompanyWorkforceRange";
 
 export const indicatorNoteMax: Record<IndicatorKey, number> = {
   remunerations: IndicateurUnComputer.prototype.getMaxNote(),
@@ -54,5 +64,111 @@ export const computeIndex = (
     points,
     pointsCalculables: max,
     index: max >= 75 ? Math.round((points / max) * 100) : undefined, // undefined means "Non calculable".
+  };
+};
+
+// TODO: use this type instead of the one in DeclarationDTO.ts
+// Make adapters DTO to this type.
+
+type ComputerDeclarationInput = {
+  entreprise: Pick<Company, "range">;
+  highRemunerations?: Pick<HighRemunerationsIndicator, "favorablePopulation" | "result">;
+  maternityLeaves?: Pick<MaternityLeavesIndicator, "notComputableReason" | "result">;
+  promotions?: Pick<PromotionsIndicator, "favorablePopulation" | "notComputableReason" | "result">;
+  remunerations?: Pick<RemunerationsIndicator, "favorablePopulation" | "notComputableReason" | "result">;
+  salaryRaises?: Pick<SalaryRaisesIndicator, "favorablePopulation" | "notComputableReason" | "result">;
+  salaryRaisesAndPromotions?: Pick<
+    SalaryRaisesAndPromotionsIndicator,
+    "favorablePopulation" | "notComputableReason" | "result"
+  >;
+};
+
+type ComputerDeclarationOutput = {
+  [K in keyof ComputerDeclarationInput]?: {
+    score: number;
+  };
+} & {
+  computablePoints: number;
+  index?: number;
+  points: number;
+};
+
+export const computeIndex2 = (input: ComputerDeclarationInput): ComputerDeclarationOutput => {
+  const result: Pick<ComputerDeclarationOutput, keyof ComputerDeclarationInput> = {};
+
+  let points = 0,
+    computablePoints = 0;
+
+  if (!input.remunerations?.notComputableReason) {
+    assert(input.remunerations?.result, "result must be set if notComputableReason is not set");
+
+    result.remunerations = {
+      score: IndicateurUnComputer.prototype.computeNote(input.remunerations.result.getValue()) || 0,
+    };
+
+    points += result.remunerations.score;
+    computablePoints += IndicateurUnComputer.prototype.getMaxNote();
+  }
+
+  if (input.entreprise.range?.getValue() === CompanyWorkforceRange.Enum.FROM_50_TO_250) {
+    if (!input.salaryRaisesAndPromotions?.notComputableReason) {
+      assert(input.salaryRaisesAndPromotions?.result, "result must be set if notComputableReason is not set");
+
+      result.salaryRaisesAndPromotions = {
+        score:
+          IndicateurDeuxTroisComputer.prototype.computeNote(input.salaryRaisesAndPromotions.result?.getValue()) || 0,
+      };
+
+      points += result.salaryRaisesAndPromotions?.score;
+      computablePoints += IndicateurDeuxTroisComputer.prototype.getMaxNote();
+    }
+  } else {
+    if (!input.salaryRaises?.notComputableReason) {
+      assert(input.salaryRaises?.result, "result must be set if notComputableReason is not set");
+
+      result.salaryRaises = {
+        score: IndicateurDeuxComputer.prototype.computeNote(input.salaryRaises.result?.getValue()) || 0,
+      };
+
+      points += result.salaryRaises?.score;
+      computablePoints += IndicateurDeuxComputer.prototype.getMaxNote();
+    }
+    if (!input.promotions?.notComputableReason) {
+      assert(input.promotions?.result, "result must be set if notComputableReason is not set");
+
+      result.promotions = {
+        score: IndicateurTroisComputer.prototype.computeNote(input.promotions.result?.getValue()) || 0,
+      };
+
+      points += result.promotions.score;
+      computablePoints += IndicateurTroisComputer.prototype.getMaxNote();
+    }
+  }
+
+  if (!input.maternityLeaves?.notComputableReason) {
+    assert(input.maternityLeaves?.result, "result must be set if notComputableReason is not set");
+
+    result.maternityLeaves = {
+      score: IndicateurQuatreComputer.prototype.computeNote(input.maternityLeaves.result?.getValue()) || 0,
+    };
+
+    points += result.maternityLeaves.score;
+    computablePoints += IndicateurQuatreComputer.prototype.getMaxNote();
+  }
+
+  assert(input.highRemunerations?.result, "result must be set if notComputableReason is not set");
+
+  result.highRemunerations = {
+    score: IndicateurCinqComputer.prototype.computeNote(input.highRemunerations?.result.getValue()) || 0,
+  };
+
+  points += result.highRemunerations.score;
+  computablePoints += IndicateurCinqComputer.prototype.getMaxNote();
+
+  return {
+    ...result,
+    points,
+    computablePoints,
+    index: computablePoints >= 75 ? Math.round((points / computablePoints) * 100) : undefined, // undefined means "Non calculable".
   };
 };
