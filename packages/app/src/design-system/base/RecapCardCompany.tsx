0e73b1b0132ab33b240064efb2d1d74b88c1a06f
jonat75 <jonathan.perrault@gmail.com>
2023-11-02 16:21:59 +0100

    feat: add edit button on recap company infos (#1791)

--- packages/app/src/design-system/base/RecapCardCompany.tsx
+++ packages/app/src/design-system/base/RecapCardCompany.tsx
@@ -4,11 +4,12 @@ import { type CompanyDTO } from "@common/core-domain/dtos/CompanyDTO";
 import { COUNTRIES_ISO_TO_LIB, NAF } from "@common/dict";
 import { ClientBodyPortal } from "@components/utils/ClientBodyPortal";
 
+import { funnelStaticConfig } from "../../app/(default)/index-egapro/declaration/declarationFunnelConfiguration";
 import { Grid, GridCol } from "./Grid";
 import { RecapCard } from "./RecapCard";
 import { Text } from "./Typography";
 
-type Props = { company: CompanyDTO; full?: boolean; title?: string };
+type Props = { company: CompanyDTO; edit?: boolean; full?: boolean; title?: string };
 
 // TODO: replace with tooltip when available in DSFR
 const infoModale = createModal({
@@ -16,7 +17,7 @@ const infoModale = createModal({
   isOpenedByDefault: false,
 });
 
-export const RecapCardCompany = ({ company, full, title }: Props) => {
+export const RecapCardCompany = ({ company, full, title, edit }: Props) => {
   const { name, address, postalCode, city, countryIsoCode, siren, nafCode, workforce, ues } = company;
 
   const titleFull = title ?? "Informations de l'entreprise déclarante";
@@ -24,6 +25,63 @@ export const RecapCardCompany = ({ company, full, title }: Props) => {
   const postalCodeCity = `${postalCode} ${city}`.trim();
   const countryLib =
     countryIsoCode && countryIsoCode !== "FR" && `${postalCodeCity && ", "}${COUNTRIES_ISO_TO_LIB[countryIsoCode]}`;
+
+  const content = full ? (
+    <>
+      <Grid haveGutters>
+        <GridCol sm={12} className="fr-pb-0">
+          <strong>Raison sociale</strong>
+          <br />
+          {name}
+        </GridCol>
+        <GridCol sm={3} className="fr-pb-0">
+          <strong>Siren</strong>
+          <br />
+          {siren}
+        </GridCol>
+        {nafCode && (
+          <GridCol sm={9} className="fr-pb-0">
+            <strong>Code NAF</strong>
+            <br />
+            {nafCode} - {NAF[nafCode].description}
+          </GridCol>
+        )}
+        <GridCol sm={12}>
+          <strong>Adresse</strong>
+          <br />
+          {address}
+          {postalCodeCity ? `, ${postalCodeCity}` : " "}
+          {countryLib}
+        </GridCol>
+      </Grid>
+    </>
+  ) : (
+    <>
+      Entreprise déclarante : <strong>{name}</strong>
+      <br />
+      {address}
+      {(postalCodeCity || countryLib) && <br />}
+      {postalCodeCity}
+      {countryLib}
+      <br />
+      Siren : <strong>{siren}</strong>
+      <br />
+      Code NAF : <strong>{nafCode}</strong> - {nafCode && NAF[nafCode].description}
+      {workforce?.range && (
+        <>
+          <br />
+          Tranche d'effectifs assujettis de l'
+          {ues?.name ? "UES" : "entreprise"} : <strong>{CompanyWorkforceRange.Label[workforce.range]}</strong>
+        </>
+      )}
+    </>
+  );
+
+  const fullTitle = full ? (
+    <Text text={titleFull} variant={["xl"]} inline />
+  ) : (
+    title ?? "Informations entreprise déclarante"
+  );
 
   return (
     <>
@@ -33,69 +91,22 @@ export const RecapCardCompany = ({ company, full, title }: Props) => {
           l'INSEE).
         </infoModale.Component>
       </ClientBodyPortal>
-      <RecapCard
-        title={full ? <Text text={titleFull} variant={["xl"]} inline /> : title ?? "Informations entreprise déclarante"}
-        sideButtonProps={{
-          iconId: "fr-icon-information-fill",
-          title: titleFull,
-          priority: "tertiary no outline",
-          style: { alignSelf: "center" },
-          size: "small",
-          nativeButtonProps: infoModale.buttonProps,
-        }}
-        content={
-          full ? (
-            <>
-              <Grid haveGutters>
-                <GridCol sm={12} className="fr-pb-0">
-                  <strong>Raison sociale</strong>
-                  <br />
-                  {name}
-                </GridCol>
-                <GridCol sm={3} className="fr-pb-0">
-                  <strong>Siren</strong>
-                  <br />
-                  {siren}
-                </GridCol>
-                {nafCode && (
-                  <GridCol sm={9} className="fr-pb-0">
-                    <strong>Code NAF</strong>
-                    <br />
-                    {nafCode} - {NAF[nafCode].description}
-                  </GridCol>
-                )}
-                <GridCol sm={12}>
-                  <strong>Adresse</strong>
-                  <br />
-                  {address}
-                  {postalCodeCity ? `, ${postalCodeCity}` : " "}
-                  {countryLib}
-                </GridCol>
-              </Grid>
-            </>
-          ) : (
-            <>
-              Entreprise déclarante : <strong>{name}</strong>
-              <br />
-              {address}
-              {(postalCodeCity || countryLib) && <br />}
-              {postalCodeCity}
-              {countryLib}
-              <br />
-              Siren : <strong>{siren}</strong>
-              <br />
-              Code NAF : <strong>{nafCode}</strong> - {nafCode && NAF[nafCode].description}
-              {workforce?.range && (
-                <>
-                  <br />
-                  Tranche d'effectifs assujettis de l'
-                  {ues?.name ? "UES" : "entreprise"} : <strong>{CompanyWorkforceRange.Label[workforce.range]}</strong>
-                </>
-              )}
-            </>
-          )
-        }
-      />
+      {edit ? (
+        <RecapCard title={fullTitle} editLink={funnelStaticConfig["entreprise"].url} content={content} />
+      ) : (
+        <RecapCard
+          title={fullTitle}
+          sideButtonProps={{
+            iconId: "fr-icon-information-fill",
+            title: titleFull,
+            priority: "tertiary no outline",
+            style: { alignSelf: "center" },
+            size: "small",
+            nativeButtonProps: infoModale.buttonProps,
+          }}
+          content={content}
+        />
+      )}
     </>
   );
 };
