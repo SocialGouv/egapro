808246e0c8cb0e29a4a6db89739ac1ead64058b4
Jonathan Perrault <jonathan.perrault@gmail.com>
2023-11-07 13:55:00 +0100

    dont let existing alert on assujetti page

--- packages/app/src/app/(default)/index-egapro/declaration/AlertExistingDeclaration.tsx
+++ packages/app/src/app/(default)/index-egapro/declaration/AlertExistingDeclaration.tsx
@@ -20,7 +20,7 @@ export const AlertExistingDeclaration = () => {
 
   const olderThanOneYear = isAfter(new Date(), add(new Date(declarationDate), { years: 1 }));
 
-  if (segment === "commencer" || segment === "confirmation") return null;
+  if (segment === "commencer" || segment === "confirmation" || segment === "assujetti") return null;
 
   return (
     <Alert
