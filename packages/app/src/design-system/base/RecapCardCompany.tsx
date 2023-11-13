df1e9c1a7d5dab756a7d059e9d75d236c0935823
Pierre-Olivier Mauguet <pierre-olivier.mauguet@beta.gouv.fr>
2023-11-13 21:48:03 +0100

    fix: closes #1785 foreign company

--- packages/app/src/design-system/base/RecapCardCompany.tsx
+++ packages/app/src/design-system/base/RecapCardCompany.tsx
@@ -22,7 +22,8 @@ export const RecapCardCompany = ({ company, full, title, edit }: Props) => {
 
   const titleFull = title ?? "Informations de l'entreprise déclarante";
 
-  const postalCodeCity = `${postalCode} ${city}`.trim();
+  // postalCode and city may be undefined for foreign companies.
+  const postalCodeCity = `${postalCode ?? ""} ${city ?? ""}`.trim();
   const countryLib =
     countryIsoCode && countryIsoCode !== "FR" && `${postalCodeCity && ", "}${COUNTRIES_ISO_TO_LIB[countryIsoCode]}`;
 
