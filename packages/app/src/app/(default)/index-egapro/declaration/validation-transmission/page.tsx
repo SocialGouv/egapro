4e9b0627c18fc7efcb8f3ed0282b743717852797
jonat75 <jonathan.perrault@gmail.com>
2023-11-16 11:55:42 +0100

    feat: declaration transmission add spaces at recap (#1844)

--- packages/app/src/app/(default)/index-egapro/declaration/validation-transmission/page.tsx
+++ packages/app/src/app/(default)/index-egapro/declaration/validation-transmission/page.tsx
@@ -14,8 +14,11 @@ const ResultatGlobalPage = () => {
       <p>
         Vous êtes sur le point de valider la procédure vous permettant de transmettre aux services du ministre chargé du
         travail vos résultats en matière d’écart de rémunération entre les femmes et les hommes conformément aux
-        dispositions de l’article D.1142-5 du code du travail. Pour terminer la procédure, cliquez sur “Valider et
-        transmettre les résultats” ci-dessous. Vous recevrez un accusé de réception.
+        dispositions de l’article D.1142-5 du code du travail.
+        <br />
+        <br />
+        Pour terminer la procédure, cliquez sur “Valider et transmettre les résultats” ci-dessous. Vous recevrez un
+        accusé de réception.
       </p>
 
       <Recap />
