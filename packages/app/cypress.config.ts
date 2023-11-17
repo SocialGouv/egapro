e4dd0e3c63e11136c3334ef246a3ad108477d536
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-11-17 13:17:28 +0100

    chore: add e2e workflow

--- packages/app/cypress.config.ts
+++ packages/app/cypress.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from "cypress";
 
-// eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  defaultCommandTimeout: 30000,
-  experimentalStudio: true,
   e2e: {
-    baseUrl: "http://localhost:3000",
-    setupNodeEvents(_on, _config) {
+    setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
