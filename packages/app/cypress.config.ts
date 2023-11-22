9b3c309cea4a2ce23654ebaf2dcb5c5108ed0603
Pierre-Olivier Mauguet <pierre-olivier.mauguet@beta.gouv.fr>
2023-11-22 15:34:27 +0100

    feat: eslint

--- packages/app/cypress.config.ts
+++ packages/app/cypress.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "cypress";
 
+// eslint-disable-next-line import/no-default-export
 export default defineConfig({
   defaultCommandTimeout: 30000,
   experimentalStudio: true,
   e2e: {
     baseUrl: "http://localhost:3000",
-    setupNodeEvents(on, config) {
+    setupNodeEvents(_on, _config) {
       // implement node event listeners here
     },
   },
