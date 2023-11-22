480c5b3a7c040211c13591da7bafe9b1bd603cf4
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-11-22 11:10:10 +0100

    fix

--- packages/app/cypress.config.ts
+++ packages/app/cypress.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  defaultCommandTimeout: 10000,
+  experimentalStudio: true,
   e2e: {
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
