95c74a46f4110f76bd5e61f7c9e9276932a8b109
Pierre-Olivier Mauguet <pierre-olivier.mauguet@beta.gouv.fr>
2023-11-30 09:36:22 +0100

    feat: start for mon espace

---
+++ packages/app/src/app/(default)/mon-espace/mon-profil/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { fr } from "@codegouvfr/react-dsfr";
+import Badge from "@codegouvfr/react-dsfr/Badge";
+import Card from "@codegouvfr/react-dsfr/Card";
+import { Box } from "@design-system";
+import { MessageProvider } from "@design-system/client";
+import { type Session } from "next-auth";
+import { useSession } from "next-auth/react";
+
+const canEditSiren = (user?: Session["user"]) => (siren?: string) => {
+  if (!siren || !user) return undefined;
+  return user.staff || user.companies.some(company => company.siren === siren);
+};
+
+const MonProfilPage = () => {
+  const session = useSession();
+
+  const user = session.data?.user;
+
+  return (
+    <MessageProvider>
+      <Box my="8w">
+        <Card
+          background
+          badge={user?.email ? <Badge severity="new">Staff</Badge> : null}
+          border
+          desc={
+            <>
+              <p className={fr.cx("fr-mt-4w")}>{`${user?.firstname} ${user?.lastname}`}</p>
+              <p>{user?.email}</p>
+            </>
+          }
+          enlargeLink
+          horizontal
+          imageAlt="texte alternatif de l’image"
+          // imageUrl="https://www.systeme-de-design.gouv.fr/img/placeholder.16x9.png"
+          imageUrl="ri-user-line"
+          linkProps={{
+            href: "#",
+          }}
+          size="medium"
+          // start={
+          //   <>
+          //     <p>{`${user?.firstname} ${user?.lastname}`}</p>
+          //     <p>{user?.email}</p>
+          //   </>
+          // }
+          title="Compte EgaPro"
+          titleAs="h3"
+        />
+      </Box>
+    </MessageProvider>
+  );
+};
+
+export default MonProfilPage;
