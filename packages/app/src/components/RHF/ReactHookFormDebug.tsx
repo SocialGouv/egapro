580e8ed04d5c6577716719ebac818b7d25f4b30f
Pierre-Olivier Mauguet <pierre-olivier.mauguet@beta.gouv.fr>
2023-11-30 16:17:19 +0100

    feat: improve RHFDebug errors display

--- packages/app/src/components/RHF/ReactHookFormDebug.tsx
+++ packages/app/src/components/RHF/ReactHookFormDebug.tsx
@@ -1,4 +1,4 @@
-import { formatZodErrors } from "@common/utils/debug";
+import { type Any } from "@common/utils/types";
 import { ClientOnly } from "@components/utils/ClientOnly";
 import { DebugButton } from "@components/utils/debug/DebugButton";
 import { type FieldValues, type FormState, useFormContext } from "react-hook-form";
@@ -7,6 +7,23 @@ interface Props {
   formState?: FormState<FieldValues>;
 }
 
+/**
+ * Recursive traversal of the errors object or array to collect all the errors.
+ */
+const collectErrors = (issues: Any, path: string[] = []): Array<[string, string]> => {
+  if (issues?.message) return [[path.join("."), issues.message]];
+
+  if (Array.isArray(issues)) {
+    return issues.flatMap((issue, index) => collectErrors(issue, [...path, String(index)]));
+  }
+
+  if (typeof issues === "object") {
+    return Object.entries(issues).flatMap(([key, value]) => collectErrors(value, [...path, key]));
+  }
+
+  return [];
+};
+
 /*
  Counter intuitive behavior of RHF: 
 
@@ -14,14 +31,16 @@ interface Props {
 
  In fact :
  - isValid is computed when the useForm's mode is set to onChange. On each keystroke, the validation is made and isValid is updated.
- - errors for a field is only updated when the user has dirty the field or when the form is submitted. So if the user has not dirty the field, the isValid will probably will be false with no error.
+ - errors for a field is only updated when the user has dirty the field or when trigger is called. So if the user has not dirty the field, the isValid will probably will be false with no error.
  */
 
 export const ReactHookFormDebug = (props: Props) => {
   const { watch, formState } = useFormContext();
 
   // Can't use ...rest because nothing is returned since formState is a Proxy and must be destructured explicitly.
-  const { errors, isValid } = props.formState || formState;
+  const { errors, isValid, isDirty } = props.formState || formState;
+
+  const presenterErrors = Object.fromEntries(collectErrors(errors));
 
   return (
     <ClientOnly>
@@ -35,9 +54,13 @@ export const ReactHookFormDebug = (props: Props) => {
           <legend>
             <DebugButton obj={errors} alwaysOn infoText="Form Errors" /> Form Errors
           </legend>
-          <pre>isValid: {JSON.stringify(isValid, null, 2)}</pre>
+          <pre>
+            isValid: {JSON.stringify(isValid, null, 2)}
+            <br />
+            isDirty: {JSON.stringify(isDirty, null, 2)}
+          </pre>
           {/* when mode is onChange, errors are only updated when the user has dirty the field or when the form is submitted. */}
-          <pre>{formatZodErrors(errors)}</pre>
+          <pre>{JSON.stringify(presenterErrors, null, 2)}</pre>
           {/* isValid is only updated when the mode of useForm is set to onChange. */}
         </fieldset>
       </fieldset>
