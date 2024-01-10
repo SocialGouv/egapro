997911fccf59a69ae125bdd38d0b857204571c99
Jonathan Perrault <jonathan.perrault@gmail.com>
2024-01-10 13:24:53 +0100

    fix: get values formState on indicator 2 or 3

--- packages/app/src/app/(default)/index-egapro/simulateur/(funnel)/(indicateurs2ou3)/Form.tsx
+++ packages/app/src/app/(default)/index-egapro/simulateur/(funnel)/(indicateurs2ou3)/Form.tsx
@@ -116,11 +116,14 @@ export const Indic2or3Form = ({ indicateur }: Indic2or3FormProps) => {
     handleSubmit,
     register,
     watch,
-    getValues,
     setValue,
     reset,
   } = methods;
 
+  const computableCheck = watch("calculable");
+  const pourcentages = watch("pourcentages");
+
+  let pourcentagesWithCount = undefined as Percentages | undefined;
   let computed = {} as ComputedResult;
 
   const canCompute = computer.canCompute();
@@ -142,11 +145,6 @@ export const Indic2or3Form = ({ indicateur }: Indic2or3FormProps) => {
       </CenteredContainer>
     );
   }
-
-  const computableCheck = watch("calculable") || getValues("calculable");
-  const pourcentages = watch("pourcentages");
-
-  let pourcentagesWithCount = undefined as Percentages | undefined;
 
   // add count from "funnel.effectifs.csp" to pourcentages, to make pourcentagesWithCount
   if (funnel?.effectifs) {
