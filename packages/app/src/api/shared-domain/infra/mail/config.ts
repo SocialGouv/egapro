ad17f59c24b4b06dbee8cc753407267030ffddcd
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2024-01-19 12:19:47 +0100

    fix: mailer config

--- packages/app/src/api/shared-domain/infra/mail/config.ts
+++ packages/app/src/api/shared-domain/infra/mail/config.ts
@@ -5,6 +5,9 @@ export const mailerConfig: SMTPConnection.Options = {
   host: config.api.mailer.host,
   port: config.api.mailer.smtp.port,
   secure: config.api.mailer.smtp.ssl,
+  tls: {
+    ciphers:'SSLv3'
+  },
   ...(config.api.mailer.smtp.login && config.api.mailer.smtp.password
     ? {
         auth: {
