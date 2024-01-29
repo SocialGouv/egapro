e50f383137ddcc26512722b2632b29224b54f7bf
Jonathan Perrault <jonathan.perrault@gmail.com>
2024-01-29 10:41:01 +0100

    fix: MCP link

--- packages/app/src/app/HeaderAccountMenu.tsx
+++ packages/app/src/app/HeaderAccountMenu.tsx
@@ -1,6 +1,5 @@
 import { fr } from "@codegouvfr/react-dsfr";
 import { cx } from "@codegouvfr/react-dsfr/tools/cx";
-import { config } from "@common/config";
 import { type Session } from "next-auth";
 
 import style from "./root.module.scss";
@@ -46,9 +45,7 @@ export const HeaderAccountMenu = (props: { session: { data: Session }; staff: bo
               <li>
                 <a
                   className="fr-nav__link"
-                  href={`https://app${
-                    config.api.security.moncomptepro.appTest ? "-test" : ""
-                  }.moncomptepro.beta.gouv.fr`}
+                  href={`https://app.moncomptepro.beta.gouv.fr`}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
