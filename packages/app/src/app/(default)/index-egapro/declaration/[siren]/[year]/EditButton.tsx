68983ae26eb6556fba4aaecd90c54ec156af64d4
jonat75 <jonathan.perrault@gmail.com>
2024-02-22 16:48:41 +0100

    fix: edit existing declaration from recap (#2151)

--- packages/app/src/app/(default)/index-egapro/declaration/[siren]/[year]/EditButton.tsx
+++ packages/app/src/app/(default)/index-egapro/declaration/[siren]/[year]/EditButton.tsx
@@ -9,17 +9,37 @@ import { add, isAfter } from "date-fns";
 import { useRouter } from "next/navigation";
 import { type PropsWithChildren } from "react";
 
+import { prepareDataWithExistingDeclaration } from "../../commencer/CommencerForm";
+import { funnelConfig } from "../../declarationFunnelConfiguration";
+
 type Props = {
   déclaration: DeclarationDTO;
+  year: number;
 };
 
-export const EditButton = ({ déclaration }: PropsWithChildren<Props>) => {
+export const EditButton = ({ déclaration, year }: PropsWithChildren<Props>) => {
   const router = useRouter();
-  const { setStatus } = useDeclarationFormManager();
+  const { setStatus, saveFormData, resetFormData } = useDeclarationFormManager();
 
   const date = déclaration["declaration-existante"].date;
 
   const olderThanOneYear = date === undefined || isAfter(new Date(), add(new Date(date), { years: 1 }));
+  const saveAndGoNext = async (
+    { siren, annéeIndicateurs }: { annéeIndicateurs: number; siren: string },
+    formData: DeclarationDTO,
+  ) => {
+    try {
+      // Synchronize the data with declaration if any.
+      const newData = await prepareDataWithExistingDeclaration(siren, annéeIndicateurs, formData);
+
+      // Save in storage (savePageData is not used because we want to save commencer page and declaration-existante).
+      saveFormData(newData);
+
+      router.push(funnelConfig(newData)["declarant"].next().url);
+    } catch (error: unknown) {
+      console.error("Unexpected API error", error);
+    }
+  };
 
   return (
     <>
@@ -30,9 +50,17 @@ export const EditButton = ({ déclaration }: PropsWithChildren<Props>) => {
         }}
         nextLabel="Modifier"
         nextProps={{
-          onClick: () => {
+          onClick: async () => {
             setStatus("edition");
-            router.push(`${config.base_declaration_url}/declarant`);
+            resetFormData();
+            await saveAndGoNext(
+              {
+                siren: déclaration.entreprise?.entrepriseDéclarante?.siren ?? "",
+                annéeIndicateurs: year,
+              },
+              déclaration,
+            );
+            //  router.push(`${config.base_declaration_url}/declarant`);
           },
           disabled: olderThanOneYear,
         }}
