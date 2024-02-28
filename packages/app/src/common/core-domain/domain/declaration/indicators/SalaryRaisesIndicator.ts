4a097e58fb7bbbb55f5d87482e3e1afaead14a07
Artur Rucinski <artur.rucinski@legratin.io>
2024-02-28 17:02:31 +0100

    fix: allow over 100% results for promotion, renumeration and raises indicators

--- packages/app/src/common/core-domain/domain/declaration/indicators/SalaryRaisesIndicator.ts
+++ packages/app/src/common/core-domain/domain/declaration/indicators/SalaryRaisesIndicator.ts
@@ -1,5 +1,10 @@
 import { type EntityPropsToJson } from "@common/shared-domain";
-import { Percentage, PositiveInteger, SimpleNumber } from "@common/shared-domain/domain/valueObjects";
+import {
+  type Percentage,
+  PositiveInteger,
+  PositiveNumber,
+  SimpleNumber,
+} from "@common/shared-domain/domain/valueObjects";
 
 import { FavorablePopulation } from "../../valueObjects/declaration/indicators/FavorablePopulation";
 import { NotComputableReasonSalaryRaises } from "../../valueObjects/declaration/indicators/NotComputableReasonSalaryRaises";
@@ -50,7 +55,7 @@ export class SalaryRaisesIndicator extends AbstractIndicator<SalaryRaisesIndicat
     if (json.notComputableReason)
       props.notComputableReason = new NotComputableReasonSalaryRaises(json.notComputableReason);
     if (json.favorablePopulation) props.favorablePopulation = new FavorablePopulation(json.favorablePopulation);
-    if (typeof json.result === "number") props.result = new Percentage(json.result);
+    if (typeof json.result === "number") props.result = new PositiveNumber(json.result);
     if (typeof json.score === "number") props.score = new PositiveInteger(json.score);
 
     return new SalaryRaisesIndicator(props) as this;
