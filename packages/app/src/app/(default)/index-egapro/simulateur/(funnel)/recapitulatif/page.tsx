4feb84a0a36213b7398a1624558cce1de0e627ee
Artur Rucinski <90777022+arturlg@users.noreply.github.com>
2024-03-27 20:00:00 +0100

    fix: Simulateur final step wording (#2245)

--- packages/app/src/app/(default)/index-egapro/simulateur/(funnel)/recapitulatif/page.tsx
+++ packages/app/src/app/(default)/index-egapro/simulateur/(funnel)/recapitulatif/page.tsx
@@ -20,6 +20,10 @@ const Indicateur5Page = () => {
         résultats obtenus aux services du ministre chargé du travail en renseignant les autres informations nécessaires
         à la déclaration en cliquant sur "Poursuivre vers la déclaration" ci-dessous.
       </p>
+      <p>
+        À noter que seules les entreprises et UES d’au moins 50 salariés à la date de l’obligation de publication de
+        l’index, soit le 1er mars, doivent transmettre leurs résultats aux services du ministère chargé du travail.
+      </p>
       <RecapSimu />
     </CenteredContainer>
   );
