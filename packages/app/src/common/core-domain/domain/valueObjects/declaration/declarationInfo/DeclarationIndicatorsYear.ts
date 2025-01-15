13094f5c353248118a3f3206e0f75c62583a4abc
Jonathan Perrault <jonathan.perrault@gmail.com>
2025-01-15 17:45:26 +0100

    fix: years indicator

--- packages/app/src/common/core-domain/domain/valueObjects/declaration/declarationInfo/DeclarationIndicatorsYear.ts
+++ packages/app/src/common/core-domain/domain/valueObjects/declaration/declarationInfo/DeclarationIndicatorsYear.ts
@@ -1,8 +1,8 @@
-import { YEARS } from "@common/dict";
+import { ALL_YEARS } from "@common/dict";
 import { TupleNumber } from "@common/shared-domain/domain/valueObjects/TupleNumber";
 
 export class DeclarationIndicatorsYear extends TupleNumber {
   constructor(value: number) {
-    super(value, YEARS);
+    super(value, ALL_YEARS);
   }
 }
