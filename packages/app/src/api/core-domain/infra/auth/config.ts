0dcf4be2dca064c3eea0a9400a3886c9f5a3f0a7
Jonathan Perrault <jonathan.perrault@gmail.com>
2025-01-20 10:33:16 +0100

    fix: redirection login

--- packages/app/src/api/core-domain/infra/auth/config.ts
+++ packages/app/src/api/core-domain/infra/auth/config.ts
@@ -200,14 +200,6 @@ export const authConfig: AuthOptions = {
       }
       return session;
     },
-
-    async redirect({ url, baseUrl }) {
-      // Allows relative callback URLs
-      if (url.startsWith("/")) return `${baseUrl}${url}`;
-      // Allows callback URLs on the same origin
-      else if (new URL(url).origin === baseUrl) return url;
-      return baseUrl + "/redirect";
-    },
   },
 };
 
