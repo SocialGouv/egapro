73f9b32e754ac02b0d9837cca514c3684f0ea9e4
devthejo <devthejo@protonmail.com>
2025-01-28 13:41:19 +0100

    chore: wip

--- packages/app/src/components/utils/SentryTest.tsx
+++ packages/app/src/components/utils/SentryTest.tsx
@@ -4,6 +4,9 @@ import { useCallback } from "react";
 
 export const SentryTest = () => {
   const triggerError = useCallback(() => {
+    console.log({
+      NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    });
     const error = new Error("Test error for Sentry integration");
     error.name = "SentryTestError";
     throw error;
