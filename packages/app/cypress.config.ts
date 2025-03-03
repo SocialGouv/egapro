7bb3f70293a74697223a227fbdadba847522df37
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-03-03 18:21:20 +0100

    chore: test new config

--- packages/app/cypress.config.ts
+++ packages/app/cypress.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   video: false,
   screenshotOnRunFailure: false,
   e2e: {
-    baseUrl: "http://localhost:3000",
+    baseUrl: process.env.TEST_BASEURL ?? "http://localhost:3000",
     setupNodeEvents(_on, _config) {
       // implement node event listeners here
     },
