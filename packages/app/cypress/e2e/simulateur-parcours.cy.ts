2bb633f42492795fac2506f291a5eacefd8ea1fd
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-03-13 15:18:43 +0100

    add load cache hack back

--- packages/app/cypress/e2e/simulateur-parcours.cy.ts
+++ packages/app/cypress/e2e/simulateur-parcours.cy.ts
@@ -6,6 +6,9 @@ describe("Parcours du simulateur Index Egapro", () => {
   });
 
   it("Doit compléter le parcours du simulateur jusqu'à la page de récapitulatif", () => {
+    cy.visit("/index-egapro/simulateur/commencer");
+    cy.url().should("include", "/effectifs");
+
     cy.intercept("GET", "/index-egapro/simulateur/commencer").as("pageLoad");
 
     // Visiter la page de démarrage du simulateur
