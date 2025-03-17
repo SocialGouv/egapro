3f46e74e103bd1bc777a79b55d763edca532c367
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-03-17 15:42:35 +0100

    refactor

--- packages/app/cypress/e2e/declaration_251-999_employees_firm_1.cy.ts
+++ packages/app/cypress/e2e/declaration_251-999_employees_firm_1.cy.ts
@@ -152,6 +152,6 @@ describe("Declaration", () => {
     });
     cy.contains("button", "Valider et transmettre les résultats").click();
     cy.contains("h1", "Votre déclaration a été transmise").should("exist");
-    cy.request("POST", "/clean-test-user/declaration");
+    cy.request("POST", "/api/clean-test-user/declaration");
   });
 });
