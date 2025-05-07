cb392cf00ea0084d559779d3bedd425e5bd81360
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-05-07 11:18:41 +0200

    Revert "test"
    This reverts commit 72dfa535a5ccd46105ee4d89f30b351ef215e76c.

--- packages/app/cypress/e2e/declaration_50-250_employees_firm_4.cy.ts
+++ packages/app/cypress/e2e/declaration_50-250_employees_firm_4.cy.ts
@@ -12,7 +12,7 @@ describe("Declaration", () => {
   it("Doit compléter le parcours du simulateur jusqu'à la page de récapitulatif", () => {
     // load cache
     cy.visit("/");
-
+    cy.visit("/index-egapro");
     cy.visit("/login");
     cy.checkUrl("/login");
     cy.get(".fr-connect").click();
@@ -39,7 +39,7 @@ describe("Declaration", () => {
     cy.wait("@pageLoad");
     cy.contains("a", "Calculer - Déclarer mon Index").click();
 
-    cy.visit("/index-egapro");
+    cy.checkUrl("/index-egapro");
     cy.get("#content").within(() => {
       cy.contains("a", "Déclarer mon index").click();
     });
