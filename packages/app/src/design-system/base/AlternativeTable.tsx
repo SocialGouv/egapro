05403a83bb0a5751ba630d909aa1bde61cd8f8f7
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-06-10 15:20:30 +0200

    fix: ts error

--- packages/app/src/design-system/base/AlternativeTable.tsx
+++ packages/app/src/design-system/base/AlternativeTable.tsx
@@ -270,7 +270,14 @@ export const AlternativeTable = (props: AlternativeTableProps) => {
                         <AlternativeTableCell key={`${row.key || index}-${j}-${k}`} align={row.alignCols ?? "right"}>
                           {isDsfrInputProps(col) ? (
                             <TooltipWrapper message={col?.stateRelatedMessage?.toString()}>
-                              <Input {...col} hideLabel classes={{ message: "fr-sr-only" }} textArea={false} />
+                              <Input
+                                label={col.label}
+                                state={col.state}
+                                stateRelatedMessage={col.stateRelatedMessage}
+                                nativeInputProps={col.nativeInputProps}
+                                hideLabel
+                                classes={{ message: "fr-sr-only" }}
+                              />
                             </TooltipWrapper>
                           ) : (
                             col
@@ -291,7 +298,14 @@ export const AlternativeTable = (props: AlternativeTableProps) => {
                       <AlternativeTableCell key={`${row.key || index}-${k}`} align={row.alignCols ?? "right"}>
                         {isDsfrInputProps(col) ? (
                           <TooltipWrapper message={col?.stateRelatedMessage?.toString()}>
-                            <Input {...col} hideLabel classes={{ message: "fr-sr-only" }} textArea={false} />
+                            <Input
+                              label={col.label}
+                              state={col.state}
+                              stateRelatedMessage={col.stateRelatedMessage}
+                              nativeInputProps={col.nativeInputProps}
+                              hideLabel
+                              classes={{ message: "fr-sr-only" }}
+                            />
                           </TooltipWrapper>
                         ) : (
                           col
