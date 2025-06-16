2397aeae72dfac51d39e5e27efba52acc970153d
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-06-16 13:31:43 +0200

    review

--- packages/app/src/design-system/base/client/AccessibleInfoIcon.tsx
+++ packages/app/src/design-system/base/client/AccessibleInfoIcon.tsx
@@ -17,12 +17,12 @@ export const AccessibleInfoIcon = ({ description, className }: AccessibleInfoIco
       <button
         className={fr.cx("fr-btn", "fr-btn--sm", "fr-btn--tooltip", "fr-p-0", "fr-mr-0")}
         aria-describedby={tooltipId}
+        type="button"
       >
-        {/* <Icon icon="fr-icon-information-fill" size="xs" color="text-mention-grey" /> */}
         <span className={fr.cx("fr-sr-only")}>plus d'informations</span>
       </button>
 
-      <span id={tooltipId} className={fr.cx("fr-tooltip", "fr-placement")} role="tooltip" aria-hidden="true">
+      <span id={tooltipId} className={fr.cx("fr-tooltip", "fr-placement")} role="tooltip">
         {description}
       </span>
     </span>
