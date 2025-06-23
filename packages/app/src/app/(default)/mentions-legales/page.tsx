3c830b6943c1dec8db78833908ff81cf5f13a6b6
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-06-23 10:45:43 +0200

    feat: rewording mention legales (#2528)
    * feat: rewording mention legales
    
    * remove useless tests
    
    * Update packages/app/src/app/(default)/mentions-legales/page.tsx
    
    correction orthographe
    
    Co-authored-by: revu-bot[bot] <194279620+revu-bot[bot]@users.noreply.github.com>
    
    * Update packages/app/src/app/(default)/mentions-legales/page.tsx
    
    Co-authored-by: revu-bot[bot] <194279620+revu-bot[bot]@users.noreply.github.com>
    
    * fix: invalid html
    
    * change space type
    
    ---------
    
    Co-authored-by: revu-bot[bot] <194279620+revu-bot[bot]@users.noreply.github.com>

--- packages/app/src/app/(default)/mentions-legales/page.tsx
+++ packages/app/src/app/(default)/mentions-legales/page.tsx
@@ -16,18 +16,16 @@ const LegalNotice = () => {
           <GridCol md={10} lg={8}>
             <h1>{title}</h1>
             <h2>Éditeur de la plateforme</h2>
-            <p>Egapro est édité par la Fabrique Numérique des Ministères sociaux située&nbsp;:</p>
+            <p>Le site est édité par la Direction générale du travail située&nbsp;:</p>
             <p>
-              Tour Mirabeau
+              14 Avenue Duquesne
               <br />
-              39-43 Quai André Citroën
+              SP 07
               <br />
-              75015 PARIS
-              <br />
-              01 40 56 60 00
+              75350&nbsp;Paris
             </p>
             <h2>Directeur de la publication</h2>
-            <p>Le directeur de la publication est Monsieur Pierre RAMAIN, directeur général du travail.</p>
+            <p>Pierre RAMAIN, directeur général du travail</p>
             <h2>Hébergement de la plateforme</h2>
             <p>La plateforme est hébergée par OVH, situé&nbsp;:</p>
             <p>
@@ -39,8 +37,16 @@ const LegalNotice = () => {
             </p>
             <h2>Accessibilité</h2>
             <p>
-              La plateforme est partiellement conforme aux normes d’accessibilité numérique, nous tâchons de la rendre
-              accessible à toutes et à tous.
+              La conformité aux normes d’accessibilité numérique est un objectif ultérieur mais nous tâchons de rendre
+              ce site accessible à toutes et à tous.
+            </p>
+            <p>
+              Si vous rencontrez un défaut d'accessibilité vous empêchant d'accéder à un contenu ou une fonctionnalité
+              du site, merci de nous en faire part via&nbsp;
+              <Link href="mailto:contact@egapro.travail.gouv.fr">contact@egapro.travail.gouv.fr</Link>.
+              <br />
+              Si vous n'obtenez pas de réponse rapide de notre part, vous êtes en droit de faire parvenir vos doléances
+              ou une demande de saisine au Défenseur des droits.
             </p>
             <h3 className="fr-text--xl">En savoir plus</h3>
             <p>
@@ -53,10 +59,6 @@ const LegalNotice = () => {
             <p>
               La plateforme est protégée par un certificat électronique, matérialisé pour la grande majorité des
               navigateurs par un cadenas. Cette protection participe à la confidentialité des échanges.
-            </p>
-            <p>
-              En aucun cas, les services associés à la plateforme ne seront à l’origine d’envoi de courriels pour
-              demander la saisie d’informations personnelles.
             </p>
           </GridCol>
         </Grid>
