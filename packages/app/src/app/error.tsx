8307e6ebd3be283949512d44510fae64d755f40e
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-10-10 09:42:44 +0200

    feat: ajout sentry dans page erreur

--- packages/app/src/app/error.tsx
+++ packages/app/src/app/error.tsx
@@ -1,15 +1,33 @@
 "use client";
 
 import Alert from "@codegouvfr/react-dsfr/Alert";
+import { captureError } from "@common/error";
 import { type NextErrorPageProps } from "@common/utils/next";
 import { DebugButton } from "@components/utils/debug/DebugButton";
 import { CenteredContainer } from "@design-system";
+import { useEffect } from "react";
 
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import style from "./root.module.scss";
 
 const ErrorPage = ({ error }: NextErrorPageProps) => {
+  useEffect(() => {
+    captureError(error, {
+      type: "client",
+      url: window.location.href,
+      path: window.location.pathname,
+      component: "ErrorPage",
+      boundary: "error.tsx",
+    });
+
+    console.log("Erreur capturée par le boundary d'erreur client:", {
+      message: error.message,
+      name: error.name,
+      stack: error.stack,
+    });
+  }, [error]);
+
   return (
     <>
       <Header />
