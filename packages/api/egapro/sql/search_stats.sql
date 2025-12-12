fe8ca372f4933259619373038505796ea79100a5
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-12-12 10:59:18 +0100

    clean api part

--- packages/api/egapro/sql/search_stats.sql
+++
@@ -1,8 +0,0 @@
-WITH subset AS (SELECT siren FROM search {where}),
-     count AS (SELECT COUNT(DISTINCT(siren)) FROM subset),
-     stats AS (SELECT avg((data->'déclaration'->>'index')::int),
-                      min((data->'déclaration'->>'index')::int),
-                      max((data->'déclaration'->>'index')::int)
-               FROM declaration
-               WHERE year=$1 AND siren IN (SELECT siren FROM subset))
-    SELECT * FROM count JOIN stats ON true;
