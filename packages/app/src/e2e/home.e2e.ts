ac9dad124edf742875573cd7197254a3e28d0902
maxgfr <25312957+maxgfr@users.noreply.github.com>
2026-02-23 12:03:16 +0100

    fix: test

--- packages/app/src/e2e/home.e2e.ts
+++ packages/app/src/e2e/home.e2e.ts
@@ -2,5 +2,5 @@ import { expect, test } from "@playwright/test";
 
 test("home page renders", async ({ page }) => {
 	await page.goto("/");
-	await expect(page.getByText("Egapro")).toBeVisible();
+	await expect(page.getByRole("heading", { name: "Bienvenue sur Egapro" })).toBeVisible();
 });
