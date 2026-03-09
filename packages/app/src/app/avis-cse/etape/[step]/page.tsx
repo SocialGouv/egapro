8797ebb2f6efc13960f810d8bd1d806a8a071ec2
Charrier <lucharrier@gmail.com>
2026-03-09 12:16:49 +0100

    feat: wire CSE opinion frontend to tRPC API (#2909)

--- packages/app/src/app/avis-cse/etape/[step]/page.tsx
+++ packages/app/src/app/avis-cse/etape/[step]/page.tsx
@@ -1,5 +1,12 @@
 import { notFound } from "next/navigation";
-import { Step1Opinions, Step2Upload, TOTAL_STEPS } from "~/modules/cseOpinion";
+import {
+	mapOpinionsFromDb,
+	Step1Opinions,
+	Step2Upload,
+	TOTAL_STEPS,
+} from "~/modules/cseOpinion";
+import { auth } from "~/server/auth";
+import { api } from "~/trpc/server";
 
 type StepPageProps = {
 	params: Promise<{ step: string }>;
@@ -14,7 +21,17 @@ export default async function CseOpinionStepPage({ params }: StepPageProps) {
 	}
 
 	if (step === 1) {
-		return <Step1Opinions />;
+		const [session, { opinions }] = await Promise.all([
+			auth(),
+			api.cseOpinion.get(),
+		]);
+		const initialData = mapOpinionsFromDb(opinions);
+		return (
+			<Step1Opinions
+				email={session?.user?.email ?? undefined}
+				initialData={initialData}
+			/>
+		);
 	}
 
 	if (step === 2) {
