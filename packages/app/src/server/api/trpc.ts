5041d777e8606c55299e6e1fa6a2babac6fddb41
Victor Zeinstra <zeinstra.victor@gmail.com>
2026-03-12 16:59:13 +0100

    fix: disconnect url config (#2973)

--- packages/app/src/server/api/trpc.ts
+++ packages/app/src/server/api/trpc.ts
@@ -79,26 +79,18 @@ export const createCallerFactory = t.createCallerFactory;
 export const createTRPCRouter = t.router;
 
 /**
- * Middleware for timing procedure execution and adding an artificial delay in development.
+ * Middleware adding an artificial delay in development.
  *
- * You can remove this if you don't like it, but it can help catch unwanted waterfalls by simulating
- * network latency that would occur in production but not in local development.
+ * Helps catch unwanted waterfalls by simulating network latency
+ * that would occur in production but not in local development.
  */
-const timingMiddleware = t.middleware(async ({ next, path }) => {
-	const start = Date.now();
-
+const timingMiddleware = t.middleware(async ({ next }) => {
 	if (t._config.isDev) {
-		// artificial delay in dev
 		const waitMs = Math.floor(Math.random() * 400) + 100;
 		await new Promise((resolve) => setTimeout(resolve, waitMs));
 	}
 
-	const result = await next();
-
-	const end = Date.now();
-	console.log(`[TRPC] ${path} took ${end - start}ms to execute`);
-
-	return result;
+	return next();
 });
 
 /**
