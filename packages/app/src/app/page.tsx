c93709de9032eeb74d39f3876456c4dd37a5ba80
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2026-03-12 18:36:38 +0100

    feat: redirect authenticated user

--- packages/app/src/app/page.tsx
+++ packages/app/src/app/page.tsx
@@ -1,7 +1,15 @@
+import { redirect } from "next/navigation";
 import { HomePage } from "~/modules/home";
+import { auth } from "~/server/auth";
 import { HydrateClient } from "~/trpc/server";
 
 export default async function Page() {
+	const session = await auth();
+
+	if (session?.user) {
+		redirect("/mon-espace");
+	}
+
 	return (
 		<HydrateClient>
 			<HomePage />
