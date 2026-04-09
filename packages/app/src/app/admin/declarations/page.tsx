74cc834ef116b40024cb4827d75950a38100aaf3
Lucas Charrier <lucharrier@gmail.com>
2026-04-09 15:35:34 +0200

    refactor(admin): remove unnecessary HydrateClient from admin pages
    No server-side prefetch is done, so HydrateClient adds no value.

--- packages/app/src/app/admin/declarations/page.tsx
+++ packages/app/src/app/admin/declarations/page.tsx
@@ -1,10 +1,5 @@
 import { AdminDeclarationsPage } from "~/modules/admin/declarations";
-import { HydrateClient } from "~/trpc/server";
 
 export default function Page() {
-	return (
-		<HydrateClient>
-			<AdminDeclarationsPage />
-		</HydrateClient>
-	);
+	return <AdminDeclarationsPage />;
 }
