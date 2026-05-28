f7e82a29a71be6acc9501f4acadbeeb8d233fce6
Victor Zeinstra <zeinstra.victor@gmail.com>
2026-05-28 17:52:03 +0200

    refactor(step6-review): move fontMedium to local SCSS module

--- packages/app/src/modules/declaration-remuneration/steps/Step6Review.tsx
+++ packages/app/src/modules/declaration-remuneration/steps/Step6Review.tsx
@@ -2,7 +2,6 @@
 
 import { useRouter } from "next/navigation";
 import { useCallback, useRef } from "react";
-import common from "~/modules/declaration-remuneration/shared/common.module.scss";
 import { computeGap, GAP_ALERT_THRESHOLD } from "~/modules/domain";
 import { getDsfrModal } from "~/modules/shared";
 import { api } from "~/trpc/react";
@@ -159,7 +158,7 @@ export function Step6Review({
 
 			<StepIndicator currentStep={6} />
 
-			<p className={`fr-mb-0 ${common.fontMedium}`}>
+			<p className={`fr-mb-0 ${stepStyles.intro}`}>
 				Vérifiez que toutes les informations ont été complétées avant de
 				soumettre votre déclaration aux services du ministère chargé du travail.
 			</p>
