6415652339f364601ee191026ef3e5307b1eb046
Lucas Charrier <lucharrier@gmail.com>
2026-06-23 16:54:27 +0200

    refactor(declaration): extract Step1WorkforceDefinition to fix file-size limit
    Step1Workforce.tsx was 410 lines (> 400 limit). Extract the DefinitionAccordion
    static content into a dedicated sub-component to bring the file to 385 lines.

--- packages/app/src/modules/declaration-remuneration/steps/Step1Workforce.tsx
+++ packages/app/src/modules/declaration-remuneration/steps/Step1Workforce.tsx
@@ -26,6 +26,7 @@ import { StepTitleRow } from "../shared/StepTitleRow";
 import { TooltipButton } from "../shared/TooltipButton";
 import type { Step1Data } from "../types";
 import styles from "./Step1Workforce.module.scss";
+import { Step1WorkforceDefinition } from "./Step1WorkforceDefinition";
 
 type Step1WorkforceProps = {
 	declarationSiren: string;
@@ -355,33 +356,7 @@ export function Step1Workforce({
 							id="accordion-step1"
 							title="Définitions et méthode de calcul"
 						>
-							<div className="fr-callout">
-								<p>Les informations affichées incluront notamment&nbsp;:</p>
-								<ul>
-									<li>Dernière situation connue&nbsp;?</li>
-									<li>Effectif physique moyen&nbsp;?</li>
-									<li>
-										Comment sont intégrés les salariés entrés et sortis&nbsp;?
-									</li>
-									<li>
-										Comment sont traités les changements de situation (ex.
-										passage du temps plein au temps partiel)&nbsp;?
-									</li>
-									<li>
-										Les absences de six mois ou plus, continues ou discontinues,
-										sont-elles exclues du calcul&nbsp;?
-									</li>
-									<li>
-										Comment sont prises en compte les absences de trois mois,
-										avec leur recalcul en équivalent temps plein (ETP)&nbsp;?
-									</li>
-									<li>
-										Le détail des règles de calcul, illustré par des exemples
-										concrets selon les types de salariés&nbsp;? (Salariés à
-										prendre en compte / Salariés à exclure)
-									</li>
-								</ul>
-							</div>
+							<Step1WorkforceDefinition />
 						</DefinitionAccordion>
 					</div>
 
