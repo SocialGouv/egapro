06919dce52789e1760648ab9dc52f001b9d16ff6
Victor Zeinstra <zeinstra.victor@gmail.com>
2026-07-01 20:08:23 +0200

    fix(a11y): contraste tag « élevé » et encarts d'avertissement (#3720) (#3758)

--- packages/app/src/modules/aide/AideCallout.tsx
+++ packages/app/src/modules/aide/AideCallout.tsx
@@ -1,4 +1,5 @@
 import { formatLongDate } from "~/modules/domain";
+import styles from "./AideCallout.module.scss";
 
 type Props = {
 	year: number;
@@ -8,7 +9,9 @@ type Props = {
 /** Callout displaying the declaration deadline for the active campaign year. */
 export function AideCallout({ year, deadline }: Props) {
 	return (
-		<div className="fr-callout fr-callout--orange-terre-battue">
+		<div
+			className={`fr-callout fr-callout--orange-terre-battue ${styles.callout}`}
+		>
 			<h2 className="fr-callout__title">Date limite de déclaration</h2>
 			<p className="fr-callout__text">
 				La date limite de déclaration pour l'année {year} est fixée au{" "}
