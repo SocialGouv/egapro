1b25d9d257f3f5ff8651ff073fbf8f34c6f515cc
Victor Zeinstra <zeinstra.victor@gmail.com>
2026-08-20 09:57:13 +0200

    fix(declaration): wording conditionnel 2e declaration sur NextStepsBox
    Deux libelles du bloc "Prochaines etapes" deviennent conditionnels a la
    prop isSecondDeclaration existante : "ont encore ete identifies" et
    "vous devez :" en 2e declaration, comportement 1ere declaration inchange.

--- packages/app/src/modules/declaration-remuneration/shared/NextStepsBox.tsx
+++ packages/app/src/modules/declaration-remuneration/shared/NextStepsBox.tsx
@@ -100,9 +100,9 @@ export function NextStepsBox({
 
 						<p className="fr-mb-0">
 							À la suite de l&apos;analyse de vos données de l&apos;indicateur
-							par catégorie de salariés, des écarts &ge; 5 % ont été identifiés.
-							Vous devez engager un des parcours de mise en conformité
-							suivant&nbsp;:
+							par catégorie de salariés, des écarts &ge; 5 % ont
+							{isSecondDeclaration ? " encore" : ""} été identifiés. Vous devez
+							engager un des parcours de mise en conformité suivant&nbsp;:
 						</p>
 
 						<ul>
@@ -120,7 +120,8 @@ export function NextStepsBox({
 						</ul>
 						<p className="fr-mb-0">
 							Si la justification n&apos;est pas possible par des critères
-							objectifs et non sexistes, vous pouvez&nbsp;:
+							objectifs et non sexistes, vous{" "}
+							{isSecondDeclaration ? "devez" : "pouvez"}&nbsp;:
 						</p>
 						<ul>
 							{!isSecondDeclaration && (
