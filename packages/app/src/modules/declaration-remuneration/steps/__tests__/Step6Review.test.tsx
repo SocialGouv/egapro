281bb7886b795f8e6c709741de37fd4d1b03e3a5
Lucas Charrier <lucharrier@gmail.com>
2026-08-31 16:30:32 +0200

    fix(declaration-remuneration): #4281 — singulariser "Indicateur par catégories de salariés" (#4397)

--- packages/app/src/modules/declaration-remuneration/steps/__tests__/Step6Review.test.tsx
+++ packages/app/src/modules/declaration-remuneration/steps/__tests__/Step6Review.test.tsx
@@ -176,7 +176,7 @@ describe("Step6Review", () => {
 			screen.getByText("Indicateurs pour l'ensemble de vos salariés"),
 		).toBeInTheDocument();
 		expect(
-			screen.getByText("Indicateurs par catégories de salariés"),
+			screen.getByText("Indicateur par catégories de salariés"),
 		).toBeInTheDocument();
 	});
 
--- packages/app/src/modules/declaration-remuneration/steps/step6/__tests__/IndicatorSections.test.tsx
+++ packages/app/src/modules/declaration-remuneration/steps/step6/__tests__/IndicatorSections.test.tsx
@@ -174,7 +174,7 @@ describe("IndicatorSections", () => {
 		);
 
 		expect(
-			screen.getByText("Indicateurs par catégories de salariés"),
+			screen.getByText("Indicateur par catégories de salariés"),
 		).toBeInTheDocument();
 		expect(
 			screen.getByText("Écart de rémunération par catégories de salariés"),
@@ -195,7 +195,7 @@ describe("IndicatorSections", () => {
 		);
 
 		expect(
-			screen.queryByText("Indicateurs par catégories de salariés"),
+			screen.queryByText("Indicateur par catégories de salariés"),
 		).not.toBeInTheDocument();
 		expect(
 			screen.queryByText("Écart de rémunération par catégories de salariés"),
